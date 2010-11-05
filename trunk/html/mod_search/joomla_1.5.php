3b10ecfe1a7c957d526b6f9d63e49a7b8265a2ae
Gary Gisclair <gary@garygisclair.com>
2010-11-05 21:57:42 +0000

    removed the label "search" from search form

--- trunk/html/mod_search/joomla_1.5.php
+++ trunk/html/mod_search/joomla_1.5.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
 <form action="<?php echo JRoute::_('index.php');?>" method="post">
 	<div class="search-module<?php echo $params->get('moduleclass_sfx') ?>">
 		<?php
-		    $output = '<label for="mod_search_searchword">'.JText::_('SEARCH').'</label><input name="searchword" id="mod_search_searchword" maxlength="'.$maxlength.'" alt="'.$button_text.'" class="inputbox'.$moduleclass_sfx.'" type="text" size="'.$width.'" value="'.$text.'" onblur="if(this.value==\'\') this.value=\''.$text.'\';" onfocus="if(this.value==\''.$text.'\') this.value=\'\';" />';
+		    $output = '<input name="searchword" id="mod_search_searchword" maxlength="'.$maxlength.'" alt="'.$button_text.'" class="inputbox'.$moduleclass_sfx.'" type="text" size="'.$width.'" value="'.$text.'" onblur="if(this.value==\'\') this.value=\''.$text.'\';" onfocus="if(this.value==\''.$text.'\') this.value=\'\';" />';
 
 			if ($button) :
 			    if ($imagebutton) :
