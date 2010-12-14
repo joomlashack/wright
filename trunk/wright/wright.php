5770209bfd6695c69a4feaba86dcece9027109e4
gnomeontherun <gnomation@gnomeontherun.com>
2010-12-14 17:08:43 +0000

    Fixing css bug - style option was replacing instead of appending to template css array

--- trunk/wright/wright.php
+++ trunk/wright/wright.php
@@ -189,11 +189,11 @@ class Wright
 
 		// Load stylesheets by scanning directory for any prefixed with an number and underscore: 1_***.cs
 		$styles['wright'] = array('reset.css', 'joomla.css', 'layout.css', 'typography.css', 'core.css');
-		$styles['template'] = JFolder::files(JPATH_THEMES.DS.$this->document->template.DS.'css', '\.css', false, false, array($this->document->template.'.css', 'ie.css', 'ie6.css', 'ie7.css', 'ie8.css', 'ie9.css', 'rtl.css'));
+		$styles['template'] = JFolder::files(JPATH_THEMES.DS.$this->document->template.DS.'css', '\d{1,2}_.*.css');
 
 		// Load up a specific style if set
 		if (is_file(JPATH_THEMES.DS.$this->document->template.DS.'css'.DS.'style-'.$this->document->params->get('style').'.css'))
-			$styles['template'] = 'style-'.$this->document->params->get('style').'.css';
+			$styles['template'][] = 'style-'.$this->document->params->get('style').'.css';
 
 		// Add some stuff for lovely IE if needed
 		if ($browser->getBrowser() == 'msie')
