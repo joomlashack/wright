0d30b59181b6cf1feebeaf2ba03e02fac263d42e
gnomeontherun <gnomation@gnomeontherun.com>
2011-04-14 20:57:50 +0000

    Removing old trunk

--- trunk/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Restrict Access to within Joomla
-defined('_JEXEC') or die('Restricted access');
-
-// Use this to include any special php into the template
--- trunk/wright/html/joomla_1.6/mod_menu/default_separator.php
+++
@@ -1,7 +0,0 @@
-<?php
-// No direct access.
-defined('_JEXEC') or die;
-
-// Note. It is important to remove spaces between elements.
-
-?><span class="separator"><?php echo $item->title;?></span>
