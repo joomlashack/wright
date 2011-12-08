bd6f679acf1632850a3c74e223f13caaa70235be
Tito Alvarez <augustoalvarez@gmail.com>
2011-12-08 00:14:11 +0000

    New chromes for flex grid with images support all over the border

--- trunk/wright/html/modules.php
+++ trunk/wright/html/modules.php
@@ -16,6 +16,43 @@ function modChrome_wrightgrid($module, &$params, &$attribs) {
 <?php
 }
 
+
+/**
+ * Wright GRID Module + Images
+ * (i.e. <jdoc:include type="modules" name="user3" grid="<?php echo $user3gridcount;?>" style="wrightgridimages" />)
+ */
+function modChrome_wrightgridimages($module, &$params, &$attribs) {
+?>
+<div class="module<?php echo $params->get( 'moduleclass_sfx' ); ?> grid_<?php echo $attribs['grid'] ?>">
+  <div class="pad4">
+  	<div class="pad5">
+  		<div class="pad6"></div>
+  	</div>
+  </div>
+  
+  <div class="pad">
+  	<div class="pad2">
+  		<div class="pad3">
+  			<div class="pad-title">
+  <?php if ($module->showtitle) : ?>
+  <h3><?php echo $module->title; ?></h3>
+  <div class="clr"></div>
+  <?php endif; ?>
+  </div>
+  <?php echo $module->content; ?>
+       </div>
+  	</div> 
+  </div>
+  <div class="pad7">
+  	<div class="pad8">
+  		<div class="pad9"></div>
+  	</div>
+  </div>
+</div>
+<?php
+}
+
+
 /**
  * SHACK GRID + Rounded Corners
  * (i.e. <jdoc:include type="modules" name="user1" grid="<?php echo $user2gridcount;?>" style="shackflexgridrounded" />)
@@ -78,6 +115,68 @@ function modChrome_wrightflexgrid($module, &$params, &$attribs) {
   <?php endif; ?>
   <?php echo $module->content; ?>
   </div>
+</div>
+<?php
+}
+
+
+/**
+ * SHACK FLEX GRID + Images
+ * (i.e. <jdoc:include type="modules" name="user1" grid="<?php echo $user2gridcount;?>" style="shackflexgridimages" />)
+ */
+function modChrome_wrightflexgridimages($module, &$params, &$attribs) {
+    $document = &JFactory::getDocument();
+	static $modulenumbera = Array();
+	if (!isset($modulenumbera[$attribs['name']]))
+		$modulenumbera[$attribs['name']] = 1;
+	
+    static $modulenumber = 1;
+    if (stripos($params->get('moduleclass_sfx'), 'flexgrid') === false) {
+        $grid = $attribs['grid'];
+        $class = $params->get('moduleclass_sfx');
+    } else {
+        $grid = preg_replace('/\D/', '', $params->get('moduleclass_sfx'));
+        $class = '';
+    }
+	$class .= ' mod_'.$modulenumbera[$attribs['name']];
+    $modulenumber++;
+	if( $modulenumbera[$attribs['name']] == 1 ) {
+        $class .= ' first';
+    }
+    if ( $modulenumbera[$attribs['name']] == $document->countModules( $attribs['name'] ) ) {
+        $class .= ' last';
+        $modulenumbera[$attribs['name']] = 0;
+    }
+    $modulenumbera[$attribs['name']]++;
+?>
+<div class="module<?php echo $class; ?> flexgrid_<?php echo $grid ?>">
+	<div class="pad4">
+		<div class="pad5">
+  		<div class="pad6"></div>
+  	</div>
+	</div>
+	
+	<div class="pad">
+		<div class="pad2">
+			<div class="pad3">
+  				<?php if ($module->showtitle) : ?>
+  					<div class="pad-title">
+						<h3><?php echo $module->title; ?></h3>
+					</div>  	
+					<div class="clr"></div>		
+  				<?php endif; ?>
+		  <?php echo $module->content; ?>
+		  <div class="clr"></div>
+  		</div>
+  	 <div class="clr"></div>
+   </div>
+    </div>
+    <div class="pad7">
+    	<div class="pad8">
+  		<div class="pad9"></div>
+  	</div>
+    </div>
+
 </div>
 <?php
 }
