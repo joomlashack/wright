2209df8b6c728ab89ed0ed8b0a6aa83ff69ffa88
TJ Baker <tj@joomlashack.com>
2011-12-12 17:15:29 +0000

    Fixed component.php file

--- trunk/component.php
+++ trunk/component.php
@@ -1,20 +1,15 @@
 <?php
-/**
- * @copyright	Copyright (C) 2005 - 2011 Joomlashack / Meritage Assets
- * @author		Jeremy Wilken - Joomlashack
- * @package		Wright
- * 
- * Single Component view file
- */
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
-<doctype>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
-<w:head />
+<jdoc:include type="head" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
 </head>
-<body>
-	<w:content />
+<body class="contentpane">
+	<jdoc:include type="message" />
+	<jdoc:include type="component" />
 </body>
 </html>
