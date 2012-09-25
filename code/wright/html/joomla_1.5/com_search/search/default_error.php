3389e40e0af10c071cdf833cfc49b6f80f6f5c0b
Tito Alvarez <augustoalvarez@gmail.com>
2012-09-25 05:41:28 -0600

    Compatibility for Joomla 3.0 Installation package compatibility with Joomla 2.5+ Removed Joomla 1.5/1.6/1.7 references and overrides

--- code/wright/html/joomla_1.5/com_search/search/default_error.php
+++
@@ -1,9 +0,0 @@
-<?php // @version $Id: default_error.php 8 2010-11-03 18:07:23Z jeremy $
-defined('_JEXEC') or die('Restricted access');
-?>
-
-<?php if($this->error): ?>
-<div class="error<?php echo $this->escape($this->params->get( 'pageclass_sfx' )) ?>">
-	<p><?php echo $this->escape($this->error); ?></p>
-</div>
-<?php endif; ?>
--- code/wright/html/joomla_1.5/mod_syndicate/default.php
+++
@@ -1,4 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<a href="<?php echo $link ?>" class="syndicate-module<?php echo $params->get('moduleclass_sfx') ?>">
-	<?php echo JHTML::_('image.site', 'livemarks.png', '/images/M_images/', NULL, NULL, 'feed-image'); ?></a>
