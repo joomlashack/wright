1475f2e154a02063cb42f0df9c87a96cbc08e984
Tito Alvarez <augustoalvarez@gmail.com>
2012-09-27 04:33:08 -0600

    Removed DS references Joomla 3.0 compatibility issues addressed

--- code/wright/parameters/joomla_2.5/presets.php
+++ code/wright/parameters/joomla_2.5/presets.php
@@ -19,7 +19,7 @@ class JFormFieldPresets extends JFormFieldList
 		$template = $this->form->getValue('template');
 		$doc->addScript(str_replace('/administrator/', '/', JURI::base()).'templates/'.$template.'/wright/parameters/assets/presets/presets.js');
 		
-		$file = simplexml_load_file(JPATH_ROOT.DS.'templates'.DS.$template.DS.'presets.xml');
+		$file = simplexml_load_file(JPATH_ROOT.'/templates'.'/'.$template.'/presets.xml');
 
 		$json = str_replace('@attributes', 'attributes', json_encode($file));
 
