e8ebb24121e7617f23c8ffe980718a862d9f8bd9
Tito Alvarez <augustoalvarez@gmail.com>
2012-11-09 10:29:02 -0600

    Fixed mod_feed compatibility for Joomla 3.0

---
+++ code/wright/html/joomla_3.0/mod_feed/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version		$Id: edit.php 21437 2011-06-04 05:23:08Z eddieajau $
+ * @package		Joomla.Site
+ * @subpackage	mod_feed
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+require('modules/mod_feed/tmpl/default.php');
