23170e08740a07ffe8ecef0910c7ed81eb6f1858
Tito Alvarez <augustoalvarez@gmail.com>
2013-02-15 08:31:36 -0600

    Fixed font-awesome reference in admin area

--- code/wright/parameters/joomla_2.5/columns.php
+++ code/wright/parameters/joomla_2.5/columns.php
@@ -13,7 +13,7 @@ class JFormFieldColumns extends JFormField
 		$doc->addScript(str_replace('/administrator/', '/', JURI::base()).'templates/'.$this->form->getValue('template').'/wright/parameters/assets/columns/columns.js');
 		$doc->addStylesheet(str_replace('/administrator/', '/', JURI::base()).'templates/'.$this->form->getValue('template').'/wright/parameters/assets/columns/columns.css');
 		$doc->addStylesheet(str_replace('/administrator/', '/', JURI::base()).'templates/'.$this->form->getValue('template').'/wright/parameters/assets/basicbootstrap.css');
-		$doc->addStylesheet(str_replace('/administrator/', '/', JURI::base()).'templates/'.$this->form->getValue('template').'/wright/fontawesomemore/css/font-awesome.css');
+		$doc->addStylesheet(str_replace('/administrator/', '/', JURI::base()).'templates/'.$this->form->getValue('template').'/wright/fontawesome/css/font-awesome.min.css');
 
 		$values = explode(';', $this->value);
 		foreach ($values as $col)
