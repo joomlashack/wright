35eda0f40bba73a38b6260bce247c62948960eb7
Tito Alvarez <tito@joomlashack.com>
2013-08-22 19:24:00 -0600

    Used regular overrides for com_finder Joomla 2.5 Fixes #87

--- code/html/com_finder/search/default_form.php
+++ code/html/com_finder/search/default_form.php
@@ -13,5 +13,5 @@ defined('_JEXEC') or die;
 $app = JFactory::getApplication();
 
 require_once(JPATH_THEMES.'/'.$app->getTemplate().'/'.'wright'.'/'.'html'.'/'.'overrider.php');
-include(Overrider::getOverride('com_finder.search'));
+include(Overrider::getOverride('com_finder.search','default_form'));
 ?>
--- code/wright/html/joomla_2.5/com_finder/search/default.php
+++ code/wright/html/joomla_2.5/com_finder/search/default.php
@@ -1,29 +1,10 @@
 <?php
 /**
- * @package		Joomla.Site
- * @subpackage	com_content
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package     Wright
+ * @subpackage  Overrider
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Joomlashack. Meritage Assets.  All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// no direct access
-defined('_JEXEC') or die;
-if (!function_exists("wright_joomla_finder_search")) :
-
-	
-	
-	
-	function wright_joomla_finder_search($buffer) {
-		$buffer = preg_replace('/ class="button"/Ui', 'class="button btn btn-primary"', $buffer);
-		$buffer = preg_replace('/<span class="small">/Ui', '<span class="label label-info small">', $buffer);
-		$buffer = preg_replace('/id="finder-search"/Ui', 'id="finder-search" class="form-inline"', $buffer);
-		
-				return $buffer;
-	}
-
-endif;
-
-ob_start("wright_joomla_finder_search");
 require('components/com_finder/views/search/tmpl/default.php');
-ob_end_flush();
-
