74935467bd169280e31ae36e48c6e1d43014eda1
Tito Alvarez <tito@joomlashack.com>
2013-08-23 19:30:59 -0600

    Standardized heading font sizes in Joomla 2.5 and 3.x Bootstrap error message only for 2.5

--- code/wright/adapters/joomla/content.php
+++ code/wright/adapters/joomla/content.php
@@ -6,15 +6,25 @@ class WrightAdapterJoomlaContent
 	{
 		$content = '';
 
-		// Checks queue for messages
-		$messages = JFactory::getApplication()->getMessageQueue();
-		if (is_array($messages) && !empty($messages)) {
-			$content .= '<div class="alert">';
-			$content .= '<a href="#" class="close" data-dismiss="alert">&times;</a>';
-			$content .= '<jdoc:include type="message" />';
-			$content .= '</div>';
+		$errorBox = false;
+		if (version_compare(JVERSION, '3.0.0', 'lt')) {
+			// Checks queue for messages
+			$messages = JFactory::getApplication()->getMessageQueue();
+			if (is_array($messages) && !empty($messages)) {
+				$errorBox = true;
+			}
 		}
 
+		if ($errorBox) {
+			$content .= '<div class="alert">';
+			$content .= '<a href="#" class="close" data-dismiss="alert">&times;</a>';
+		}
+
+		$content .= '<jdoc:include type="message" />';
+
+		if ($errorBox) {
+			$content .= '</div>';
+		}
 
 		$content .= '<jdoc:include type="component" />';
 		
