e923cf6816cad624511c2bade1d15a725cbcad02
José Markos <markos@joomlashack.com>
2013-11-21 15:11:00 -0600

    adding comments of Wight changes and joomla version base

--- code/wright/html/joomla_2.5/mod_articles_news/horizontal.php
+++ code/wright/html/joomla_2.5/mod_articles_news/horizontal.php
@@ -1,19 +1,21 @@
 <?php
+// Wright v.3 Override: Joomla 2.5.16
 /**
  * @package		Joomla.Site
- * @subpackage	mod_articles_news
+ * @subpackage	mod_articles_category
  * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // no direct access
 defined('_JEXEC') or die;
+
 ?>
 
-<ul class="unstyled newsflash-horiz<?php echo $params->get('moduleclass_sfx'); ?> row-fluid">
+<ul class="newsflash-horiz<?php echo $params->get('moduleclass_sfx') . "unstyled row-fluid"; // Wright v.3: Added row-fluid  class ?>">
 <?php for ($i = 0, $n = count($list); $i < $n; $i ++) :
 	$item = $list[$i]; ?>
-	<li class="span3">
+	<li class="span3"> <?php // Wright v.3: Added span3 class for 4 columns ?>
 	<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item');
 
 	if ($n > 1 && (($i < $n - 1) || $params->get('showLastSeparator'))) : ?>
