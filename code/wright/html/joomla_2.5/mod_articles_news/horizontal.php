3f0b593148c48d3b1a9f094b88c5d5d6ce9da95e
Tito Alvarez <augustoalvarez@gmail.com>
2013-12-10 07:22:38 -0600

    Comments and structure in newsflash override

--- code/wright/html/joomla_2.5/mod_articles_news/horizontal.php
+++ code/wright/html/joomla_2.5/mod_articles_news/horizontal.php
@@ -10,52 +10,41 @@
 // no direct access
 defined('_JEXEC') or die;
 
+$cont = 0; // Wright v.3: Counter variable to get 4 horizontal columns
 ?>
 
-<?php
-	$cont = 0; // Wright v.3: Added cont variable for close row-fluid element
-?>
+<div class="newsflash-horiz<?php echo $params->get('moduleclass_sfx'); // Wright v.3: Changed ul for div element ?>">
+	<?php for ($i = 0, $n = count($list); $i < $n; $i ++) :
+		$item = $list[$i]; ?>
 
-<?php // Wright v.3: ?>
+		<?php // Wright v.3: Added row-fluid for each horizontal set of 4 columns ?>
+			<?php if ($cont % 4 ==  0):?>
+				<div class="row-fluid">
+			<?php endif; ?>
+		<?php // End Wright v.3: Added row-fluid for each horizontal set of 4 columns ?>
 
-	<div class="newsflash-horiz<?php echo $params->get('moduleclass_sfx'); // Wright v.3: Changed ul for div element ?>">
-		<?php for ($i = 0, $n = count($list); $i < $n; $i ++) :
-			$item = $list[$i]; ?>
+			<div class="span3"> 		<?php // Wright v.3: Added span3 class for each column ?>
 
-			<?php // Wright v.3: ?>
-				<?php if ($cont % 4 ==  0):?>
-					<div class="row-fluid">
+				<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item');
+
+				if ($n > 1 && (($i < $n - 1) || $params->get('showLastSeparator'))) : ?>
+
+					<span class="article-separator">&#160;</span>
+
 				<?php endif; ?>
-			<?php // End Wright v.3: Added row-fluid wrapp ?>
 
-			<?php // Wright v.3: ?>
-				<div class="span3">
+			</div>						<?php // Wright v.3: Added span3 class for each column ?>
 
-					<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item');
+		<?php /* Wright v.3: Close row-fluid */ ?>
 
-					if ($n > 1 && (($i < $n - 1) || $params->get('showLastSeparator'))) : ?>
-
-						<span class="article-separator">&#160;</span>
-
-					<?php endif; ?>
-
+			<?php if ($cont % 4 ==  3 || $cont == $n - 1): ?>
 				</div>
-			<?php // End Wright v.3: Added span3 class for 4 columns ?>
+			<?php endif; ?>
 
-			<?php // Wright v.3: ?>
+			<?php
+				$cont = $cont + 1;
+			?>
+		<?php /* End Wright v.3: Close row-fluid */ ?>
 
-				<?php if ($cont % 4 ==  3 || $cont == $n - 1): ?>
-					</div>
-				<?php endif; ?>
-
-				<?php
-					$cont = $cont + 1;
-				?>
-
-			<?php // End Wright v.3: Added close of row-fluid wrapp and  increase $cont variable ?>
-
-		<?php endfor; ?>
-	</div>
-
-<?php // End Wright v.3: Changed structure list for flex grid of Bootstrap ?>
-
+	<?php endfor; ?>
+</div> <?php // Wright v.3: Changed ul for div element ?>
