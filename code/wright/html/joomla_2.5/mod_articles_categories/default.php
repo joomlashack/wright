c59f63a0753796bbf9e12e3ad32347479a5e5150
julioalberto64 <info@nworldt.net>
2014-07-09 00:40:08 -0600

    add horizontal option in mod articles categories

--- code/wright/html/joomla_2.5/mod_articles_categories/default.php
+++ code/wright/html/joomla_2.5/mod_articles_categories/default.php
@@ -7,10 +7,22 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+$wrightHorizontal = (isset($wrightHorizontal) ? $wrightHorizontal : true);  // Wright v.3: Enable positio horizontal parameter
+
 // no direct access
 defined('_JEXEC') or die;
 ?>
-<ul class="categories-module<?php echo $moduleclass_sfx; ?> nav nav-list">  <?php // Wright v.3: Added nav nav-list classes ?>
-<?php
-require JModuleHelper::getLayoutPath('mod_articles_categories', $params->get('layout', 'default').'_items');
-?></ul>
+
+<?php if($wrightHorizontal){ ?>
+	<div class="container-fluid">
+   		<?php
+			require JModuleHelper::getLayoutPath('mod_articles_categories', $params->get('layout', 'default').'_items');
+		?>
+	</div>
+<?php }else{ ?>
+	<ul class="categories-module<?php echo $moduleclass_sfx; ?> nav nav-list">  <?php // Wright v.3: Added nav nav-list classes ?>
+		<?php
+		require JModuleHelper::getLayoutPath('mod_articles_categories', $params->get('layout', 'default').'_items');
+		?>
+	</ul>
+<?php } ?>
