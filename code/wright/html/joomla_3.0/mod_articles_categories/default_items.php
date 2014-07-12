bb0aaec8f10767fd20f35f9f1e5fa6602a2a933c
Tito Alvarez <augustoalvarez@gmail.com>
2014-07-12 12:25:00 -0600

    TPLUNLIMITED-208 Added horizontal override for categories in Joomla 3

--- code/wright/html/joomla_3.0/mod_articles_categories/default_items.php
+++ code/wright/html/joomla_3.0/mod_articles_categories/default_items.php
@@ -9,8 +9,85 @@
  */
 
 defined('_JEXEC') or die;
+
+$wrightMaxColumns = (isset($wrightMaxColumns) ? $wrightMaxColumns : 3);  // Wright v.3: Max columns to be used
+$wrightHorizontal = (isset($wrightHorizontal) ? $wrightHorizontal : false);  // Wright v.3: Horizontal view
+$wrightHorizontalLinkedDescriptions = (isset($wrightHorizontalLinkedDescriptions) ? $wrightHorizontalLinkedDescriptions : false);  // Wright v.3: Link categories on horizontal view
+/* Wright v.3: Grab parameter for max column number, setting it to one of the allowed Bootstrap values */
+if ($wrightMaxColumns > 6) {
+	$wrightMaxColumns = 6;
+}
+
+$span = (int)(12 / $wrightMaxColumns);
+/* End Wright v.3: Grab parameter for max column number */
+
+$c = 0; // Wright v.3: Counter variable to get horizontal columns (set by $wrightMaxColumns)
+
 foreach ($list as $item) :
 
+	/* Wright v.3: If horizontal display is enabled, displays categories showing category image (when available) and in horizontal / column layout */
+ if ($wrightHorizontal) {
+ ?>
+<?php if ($c % $wrightMaxColumns ==  0):?>
+	<div class="row-fluid">
+<?php endif; ?>
+<div class="span<?php echo $span ?>" <?php if ($_SERVER['PHP_SELF'] == JRoute::_(ContentHelperRoute::getCategoryRoute($item->id))) echo ' class="active"';?>> <?php $levelup=$item->level-$startLevel -1; ?>
+  <?php if($item->getParams()->get('image')){  ?>
+  	<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($item->id)); ?>">
+		<img src="<?php echo $item->getParams()->get('image'); ?>" class="img-block">
+	</a>
+ <?php } ?>
+<span class="category-title">
+  <h<?php echo $params->get('item_heading')+ $levelup; ?>>
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($item->id)); ?>">
+		<?php echo $item->title;?></a>
+   </h<?php echo $params->get('item_heading')+ $levelup; ?>>
+</span>
+		<?php
+		if($params->get('show_description', 0)) :
+		?>
+			<span class="category-separator">
+				-
+			</span>
+			<span class="category-description">
+		<?php
+			if ($wrightHorizontalLinkedDescriptions) :
+		?>
+			<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($item->id)); ?>">
+		<?php
+			endif;
+			echo JHtml::_('content.prepare', $item->description, $item->getParams(), 'mod_articles_categories.content');
+			if ($wrightHorizontalLinkedDescriptions) :
+		?>
+			</a>
+		<?php
+			endif;
+		?>
+			</span>
+		<?php
+		endif;
+		if($params->get('show_children', 0) && (($params->get('maxlevel', 0) == 0) || ($params->get('maxlevel') >= ($item->level - $startLevel))) && count($item->getChildren()))
+		{
+
+			echo '<ul class="nav nav-list">';  // Wright v.3: Added nav nav-list classes
+			$temp = $list;
+			$list = $item->getChildren();
+			require JModuleHelper::getLayoutPath('mod_articles_categories', $params->get('layout', 'default').'_items');
+			$list = $temp;
+			echo '</ul>';
+		}
+		?>
+ </div>
+ <?php if ($c % $wrightMaxColumns == ($wrightMaxColumns-1) || (count($list)-1) == $c): ?>
+	</div>
+<?php endif; ?>
+ <?php
+	$c = $c + 1;
+?>
+<?php }
+ else
+ 	{
+	/* End Wright v.3: If horizontal display is enabled, displays categories showing category image (when available) and in horizontal / column layout */
 ?>
 	<li <?php if ($_SERVER['PHP_SELF'] == JRoute::_(ContentHelperRoute::getCategoryRoute($item->id))) echo ' class="active"';?>> <?php $levelup = $item->level - $startLevel - 1; ?>
   <h<?php echo $params->get('item_heading') + $levelup; ?>>
@@ -38,4 +115,6 @@ foreach ($list as $item) :
 		}
 		?>
  </li>
+<?php } // Wright v.3: If horizontal display is enabled, displays categories showing category image (when available) and in horizontal / column layout */
+ ?>
 <?php endforeach; ?>
