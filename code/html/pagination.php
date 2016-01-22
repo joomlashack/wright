3e4fa5d2f4f8363fef7f0ef236df42833402d84c
jmarkos <markos@joomlashack.com>
2016-01-22 13:13:30 -0600

    fixing warning of pagination (reverted from commit 747ed65b8564e4ab2015bffa56e89ecacbb5c149)

---
+++ code/html/pagination.php
@@ -0,0 +1,6 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+// Include default pagination
+$app = JFactory::getApplication();
+include(JPATH_THEMES.'/'.$app->getTemplate().'/wright/html/pagination.php');
