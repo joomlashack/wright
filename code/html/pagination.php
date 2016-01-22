7046d62c81ea9cd6a313312b2ad7a6c9480e7ce2
jmarkos <markos@joomlashack.com>
2016-01-22 13:32:53 -0600

    eliminating the pagination.php file called

--- code/html/pagination.php
+++ code/html/pagination.php
@@ -3,4 +3,3 @@ defined('_JEXEC') or die('Restricted access');
 
 // Include default pagination
 $app = JFactory::getApplication();
-include(JPATH_THEMES.'/'.$app->getTemplate().'/wright/html/pagination.php');
