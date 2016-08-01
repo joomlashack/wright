eb6dc03ea99c7f9243eda5cde591297c8fe94c32
Valentín <htmgarcia@gmail.com>
2016-08-01 13:30:37 -0500

    Move tags outside info container

--- code/wright/html/joomla_3.0/com_content/article/default.php
+++ code/wright/html/joomla_3.0/com_content/article/default.php
@@ -46,7 +46,7 @@ $canEdit = $params->get('access-edit');
 $user    = JFactory::getUser();
 $info    = $params->get('info_block_position', 0);
 JHtml::_('behavior.caption');
-$useDefList = ($params->get('show_tags') || $params->get('show_modify_date') || $params->get('show_publish_date') || $params->get('show_create_date')
+$useDefList = ($params->get('show_modify_date') || $params->get('show_publish_date') || $params->get('show_create_date')
 	|| $params->get('show_hits') || $params->get('show_category') || $params->get('show_parent_category') || $params->get('show_author'));
 
 ?>
@@ -150,13 +150,12 @@ if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->
 /* End Wright v.3: Item elements structure */
 ?>
 
-	<?php if ($useDefList && ($info == 0 || $info == 2)) : ?>
+    <?php if ($params->get('show_tags', 1) && !empty($this->item->tags)) : ?>
+        <?php $this->item->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
+        <?php echo $this->item->tagLayout->render($this->item->tags->itemTags); ?>
+    <?php endif; ?>
 
-        <?php if ($params->get('show_tags', 1) && !empty($this->item->tags)) : ?>
-            <?php $this->item->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
-            <?php echo $this->item->tagLayout->render($this->item->tags->itemTags); ?>
-        <?php endif; ?>
-
+     <?php if ($useDefList && ($info == 0 || $info == 2)) : ?>
 		<div class="article-info muted">
 			<dl class="article-info">
 			<dt class="article-info-term"><?php echo JText::_('COM_CONTENT_ARTICLE_INFO'); ?></dt>
@@ -305,13 +304,12 @@ article_info_bottom:
 		if ($params->get('access-view')):   // access-view
 /* End Wright v.3: Item elements structure */
 ?>
-	<?php if ($useDefList && ($info == 1 || $info == 2)) : ?>
+    <?php if ($params->get('show_tags', 1) && !empty($this->item->tags)) : ?>
+        <?php $this->item->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
+        <?php echo $this->item->tagLayout->render($this->item->tags->itemTags); ?>
+    <?php endif; ?>
 
-        <?php if ($params->get('show_tags', 1) && !empty($this->item->tags)) : ?>
-            <?php $this->item->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
-            <?php echo $this->item->tagLayout->render($this->item->tags->itemTags); ?>
-        <?php endif; ?>
-
+     <?php if ($useDefList && ($info == 1 || $info == 2)) : ?>
 		<div class="article-info muted">
 			<dl class="article-info">
 			<dt class="article-info-term"><?php echo JText::_('COM_CONTENT_ARTICLE_INFO'); ?></dt>
