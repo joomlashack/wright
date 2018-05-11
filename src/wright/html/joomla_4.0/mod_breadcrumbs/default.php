788431241dfb0a5912d39d71bd12b545a2abd4d1
Valentín García <htmgarcia@gmail.com>
2018-05-11 11:51:54 -0500

    New breadcrumbs code

--- src/wright/html/joomla_4.0/mod_breadcrumbs/default.php
+++ src/wright/html/joomla_4.0/mod_breadcrumbs/default.php
@@ -9,71 +9,56 @@
  */
 
 defined('_JEXEC') or die;
-
-JHtml::_('bootstrap.tooltip');
-
-
-$separator = '<i class="icon-caret-right"></i>';  // Wright v.3: Joomla 3.x separator style
-
 ?>
 
-<ul class="breadcrumb<?php echo $moduleclass_sfx; ?>">
-	<?php
-	if ($params->get('showHere', 1))
-	{
-		echo '<li class="active">' . JText::_('MOD_BREADCRUMBS_HERE') . '&#160;</span></li>';
-	}
-	else
-	{
-		echo '<li class="active"><span class="divider icon-map-marker"></span></li>';  // Wright v.3: Changed icon-location for icon-map-marker
-	}
+<ol itemscope itemtype="https://schema.org/BreadcrumbList" class="breadcrumb">
+    <?php if ($params->get('showHere', 1)) : ?>
+        <li class="float-left">
+            <?php echo JText::_('MOD_BREADCRUMBS_HERE'); ?>&#160;
+        </li>
+    <?php else : ?>
+        <li class="float-left">
+            <span class="divider fa fa-location" aria-hidden="true"></span>
+        </li>
+    <?php endif; ?>
 
-	// Get rid of duplicated entries on trail including home page when using multilanguage
-	for ($i = 0; $i < $count; $i++)
-	{
-		if ($i == 1 && !empty($list[$i]->link) && !empty($list[$i - 1]->link) && $list[$i]->link == $list[$i - 1]->link)
-		{
-			unset($list[$i]);
-		}
-	}
+    <?php
+    // Get rid of duplicated entries on trail including home page when using multilanguage
+    for ($i = 0; $i < $count; $i++)
+    {
+        if ($i === 1 && !empty($list[$i]->link) && !empty($list[$i - 1]->link) && $list[$i]->link === $list[$i - 1]->link)
+        {
+            unset($list[$i]);
+        }
+    }
 
-	// Find last and penultimate items in breadcrumbs list
-	end($list);
-	$last_item_key = key($list);
-	prev($list);
-	$penult_item_key = key($list);
+    // Find last and penultimate items in breadcrumbs list
+    end($list);
+    $last_item_key   = key($list);
+    prev($list);
+    $penult_item_key = key($list);
 
-	// Make a link if not the last item in the breadcrumbs
-	$show_last = $params->get('showLast', 1);
+    // Make a link if not the last item in the breadcrumbs
+    $show_last = $params->get('showLast', 1);
 
-	// Generate the trail
-	foreach ($list as $key => $item) :
-	if ($key != $last_item_key)
-	{
-		// Render all but last item - along with separator
-		echo '<li>';
-		if (!empty($item->link))
-		{
-			echo '<a href="' . $item->link . '" class="pathway">' . $item->name . '</a>';
-		}
-		else
-		{
-			echo '<span>' . $item->name . '</span>';
-		}
-
-		if (($key != $penult_item_key) || $show_last)
-		{
-			echo '<span class="divider">' . $separator . '</span>';
-		}
-
-		echo '</li>';
-	}
-	elseif ($show_last)
-	{
-		// Render last item if reqd.
-		echo '<li class="active">';
-		echo '<span>' . $item->name . '</span>';
-		echo '</li>';
-	}
-	endforeach; ?>
-</ul>
+    // Generate the trail
+    foreach ($list as $key => $item) :
+        if ($key !== $last_item_key) :
+            if (!empty($item->link)) :
+                $breadcrumbItem = '<a itemprop="item" href="' . $item->link . '" class="pathway"><span itemprop="name">' . $item->name . '</span></a>';
+            else :
+                $breadcrumbItem = '<span itemprop="name">' . $item->name . '</span>';
+            endif;
+            // Render all but last item - along with separator ?>
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="breadcrumb-item"><?php echo $breadcrumbItem; ?>
+                <meta itemprop="position" content="<?php echo $key + 1; ?>">
+            </li>
+        <?php elseif ($show_last) :
+            $breadcrumbItem = '<span itemprop="name">' . $item->name . '</span>';
+            // Render last item if reqd. ?>
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="breadcrumb-item active"><?php echo $breadcrumbItem; ?>
+                <meta itemprop="position" content="<?php echo $key + 1; ?>">
+            </li>
+        <?php endif;
+    endforeach; ?>
+</ol>
