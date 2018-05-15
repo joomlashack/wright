3efaa3a8da273208695fd773196930c840ea0801
Valentín García <htmgarcia@gmail.com>
2018-05-15 08:09:39 -0500

    Layout overrides for dates elements

--- src/wright/html/joomla_4.0/layouts/joomla/content/info_block/publish_date.php
+++ src/wright/html/joomla_4.0/layouts/joomla/content/info_block/publish_date.php
@@ -1,17 +1,18 @@
 <?php
-// Wright v.3 Override: Joomla 3.2.2
+// Wright v.4 Override: Joomla 4.0
 /**
  * @package     Joomla.Site
  * @subpackage  Layout
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('JPATH_BASE') or die;
 ?>
-			<dd class="published">
-				<span class="icon-calendar"></span> <?php echo '<span class="hidden-phone"> ' . JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3'))) . '</span>';  // Wright v.3: Non-mobile version
-					echo '<span class="visible-phone"> ' . JText::sprintf(JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3'))) . '</span>';  // Wright v.3: Mobile version
-				?>
-			</dd>
+<dd class="published">
+    <span class="icon-calendar"></span>
+    <time datetime="<?php echo HTMLHelper::_('date', $displayData['item']->publish_up, 'c'); ?>" itemprop="datePublished">
+        <?php echo Text::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', HTMLHelper::_('date', $displayData['item']->publish_up, Text::_('DATE_FORMAT_LC3'))); ?>
+    </time>
+</dd>
