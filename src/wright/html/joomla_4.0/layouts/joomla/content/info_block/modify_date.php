9a209a350ac7852eb1b5f751c70223cb5b1d2862
Valentín García <htmgarcia@gmail.com>
2018-05-15 08:10:13 -0500

    Revert "Layout overrides for dates elements"
    This reverts commit 3efaa3a8da273208695fd773196930c840ea0801.

--- src/wright/html/joomla_4.0/layouts/joomla/content/info_block/modify_date.php
+++ src/wright/html/joomla_4.0/layouts/joomla/content/info_block/modify_date.php
@@ -1,19 +1,20 @@
 <?php
-// Wright v.4 Override: Joomla 4.0
+// Wright v.3 Override: Joomla 3.2.2
 /**
  * @package     Joomla.Site
  * @subpackage  Layout
  *
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('JPATH_BASE') or die;
 
 ?>
-<dd class="modified">
-    <span class="icon-calendar"></span> <?php // Wright v.4: Changed icon ?>
-    <time datetime="<?php echo HTMLHelper::_('date', $displayData['item']->modified, 'c'); ?>" itemprop="dateModified">
-        <?php echo Text::sprintf('COM_CONTENT_LAST_UPDATED', HTMLHelper::_('date', $displayData['item']->modified, Text::_('DATE_FORMAT_LC3'))); ?>
-    </time>
-</dd>
+			<dd class="modified">
+				<span class="icon-edit"></span> <?php // Wright v.3: Changed icon ?>
+				<?php
+					echo '<span class="hidden-phone"> ' . JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $displayData['item']->modified, JText::_('DATE_FORMAT_LC3'))) . '</span>';  // Wright v.3: Non-mobile version
+					echo '<span class="visible-phone"> ' . JText::sprintf(JHtml::_('date', $displayData['item']->modified, JText::_('DATE_FORMAT_LC3'))) . '</span>';  // Wright v.3: Mobile version
+				?>
+			</dd>
