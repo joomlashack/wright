3195a85687dd66a82158fd33e069616ab85f842b
Valentín García <htmgarcia@gmail.com>
2018-05-15 16:00:47 -0500

    Update credits

--- src/wrighttemplate.php
+++ src/wrighttemplate.php
@@ -3,7 +3,7 @@
  * @package     {templateName}
  * @subpackage  TemplateBase
  *
- * @copyright   Copyright (C) 2005 - 2017 Joomlashack. Meritage Assets.  All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Joomlashack.   All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
