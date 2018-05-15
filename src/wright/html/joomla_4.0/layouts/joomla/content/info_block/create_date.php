ccc3ccbf4e1411a740db440aae756433ce3aa008
Valentín García <htmgarcia@gmail.com>
2018-05-15 08:10:37 -0500

    Revert "Revert "Layout overrides for dates elements""
    This reverts commit 9a209a350ac7852eb1b5f751c70223cb5b1d2862.

--- src/wright/html/joomla_4.0/layouts/joomla/content/info_block/create_date.php
+++ src/wright/html/joomla_4.0/layouts/joomla/content/info_block/create_date.php
@@ -1,19 +1,19 @@
 <?php
-// Wright v.3 Override: Joomla 3.2.2
+// Wright v.4 Override: Joomla 4.0
 /**
  * @package     Joomla.Site
  * @subpackage  Layout
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('JPATH_BASE') or die;
 
 ?>
-			<dd class="create">
-					<span class="icon-pencil"></span> <?php // Wright v.3: Changed icon ?>
-					<?php echo '<span class="hidden-phone"> ' . JText::sprintf('COM_CONTENT_CREATED_DATE_ON', JHtml::_('date', $displayData['item']->created, JText::_('DATE_FORMAT_LC3'))) . '</span>';  // Wright v.3: Non-mobile version
-						echo '<span class="visible-phone"> ' . JText::sprintf(JHtml::_('date', $displayData['item']->created, JText::_('DATE_FORMAT_LC3'))) . '</span>';  // Wright v.3: Mobile version
-					?>
-			</dd>
+<dd class="create">
+    <span class="icon-calendar"></span> <?php // Wright v.4: Changed icon ?>
+    <time datetime="<?php echo HTMLHelper::_('date', $displayData['item']->created, 'c'); ?>" itemprop="dateCreated">
+        <?php echo Text::sprintf('COM_CONTENT_CREATED_DATE_ON', HTMLHelper::_('date', $displayData['item']->created, Text::_('DATE_FORMAT_LC3'))); ?>
+    </time>
+</dd>
