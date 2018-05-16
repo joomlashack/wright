0d74d19e55e87c731a1eedca618862fb73c871c9
Valentín García <htmgarcia@gmail.com>
2018-05-16 14:07:24 -0500

    New single contact override

--- src/wright/html/joomla_4.0/com_contact/contact/default_address.php
+++ src/wright/html/joomla_4.0/com_contact/contact/default_address.php
@@ -1,5 +1,5 @@
 <?php
-// Wright v.3 Override: Joomla 3.2.2
+// Wright v.4 Override: Joomla 4.0
 /**
  * @package     Joomla.Site
  * @subpackage  com_contact
@@ -11,120 +11,121 @@
 defined('_JEXEC') or die;
 
 /**
- * marker_class: Class based on the selection of text, none, or icons
+ * Marker_class: Class based on the selection of text, none, or icons
  * jicon-text, jicon-none, jicon-icon
  */
 ?>
-<dl class="contact-address dl-horizontal">
-	<?php if (($this->params->get('address_check') > 0) &&
-		($this->contact->address || $this->contact->suburb  || $this->contact->state || $this->contact->country || $this->contact->postcode)) : ?>
-		<?php if ($this->params->get('address_check') > 0) : ?>
-			<dt>
-				<span class="<?php echo $this->params->get('marker_class'); ?>" >
-					<i class="icon-map-marker"></i>
-					<?php /*echo $this->params->get('marker_address'); Wright v.3: replaced image for icon*/ ?>
-				</span>
-			</dt>
-		<?php endif; ?>
-
-		<?php if ($this->contact->address && $this->params->get('show_street_address')) : ?>
-			<dd>
-				<span class="contact-street">
-					<?php echo $this->contact->address .'<br/>'; ?>
-				</span>
-			</dd>
-		<?php endif; ?>
-
-		<?php if ($this->contact->suburb && $this->params->get('show_suburb')) : ?>
-			<dd>
-				<span class="contact-suburb">
-					<?php echo $this->contact->suburb .'<br/>'; ?>
-				</span>
-			</dd>
-		<?php endif; ?>
-		<?php if ($this->contact->state && $this->params->get('show_state')) : ?>
-			<dd>
-				<span class="contact-state">
-					<?php echo $this->contact->state . '<br/>'; ?>
-				</span>
-			</dd>
-		<?php endif; ?>
-		<?php if ($this->contact->postcode && $this->params->get('show_postcode')) : ?>
-			<dd>
-				<span class="contact-postcode">
-					<?php echo $this->contact->postcode .'<br/>'; ?>
-				</span>
-			</dd>
-		<?php endif; ?>
-		<?php if ($this->contact->country && $this->params->get('show_country')) : ?>
-		<dd>
-			<span class="contact-country">
-				<?php echo $this->contact->country .'<br/>'; ?>
+<dl class="contact-address dl-horizontal" itemprop="address" itemscope itemtype="https://schema.org/PostalAddress">
+    <?php if (($this->params->get('address_check') > 0) &&
+        ($this->contact->address || $this->contact->suburb  || $this->contact->state || $this->contact->country || $this->contact->postcode)) : ?>
+        <dt>
+			<span class="<?php echo $this->params->get('marker_class'); ?>">
+				<?php echo $this->params->get('marker_address'); ?>
 			</span>
-		</dd>
-		<?php endif; ?>
-	<?php endif; ?>
+        </dt>
 
-<?php if ($this->contact->email_to && $this->params->get('show_email')) : ?>
-	<dt>
-		<span class="<?php echo $this->params->get('marker_class'); ?>" >
-			<i class="icon-envelope"></i> <?php /*echo $this->params->get('marker_email');  Wright v.3: replaced image for icon */ ?>
+        <?php if ($this->contact->address && $this->params->get('show_street_address')) : ?>
+            <dd>
+				<span class="contact-street" itemprop="streetAddress">
+					<?php echo nl2br($this->contact->address); ?>
+                    <br>
+				</span>
+            </dd>
+        <?php endif; ?>
+
+        <?php if ($this->contact->suburb && $this->params->get('show_suburb')) : ?>
+            <dd>
+				<span class="contact-suburb" itemprop="addressLocality">
+					<?php echo $this->contact->suburb; ?>
+                    <br>
+				</span>
+            </dd>
+        <?php endif; ?>
+        <?php if ($this->contact->state && $this->params->get('show_state')) : ?>
+            <dd>
+				<span class="contact-state" itemprop="addressRegion">
+					<?php echo $this->contact->state; ?>
+                    <br>
+				</span>
+            </dd>
+        <?php endif; ?>
+        <?php if ($this->contact->postcode && $this->params->get('show_postcode')) : ?>
+            <dd>
+				<span class="contact-postcode" itemprop="postalCode">
+					<?php echo $this->contact->postcode; ?>
+                    <br>
+				</span>
+            </dd>
+        <?php endif; ?>
+        <?php if ($this->contact->country && $this->params->get('show_country')) : ?>
+            <dd>
+			<span class="contact-country" itemprop="addressCountry">
+				<?php echo $this->contact->country; ?>
+                <br>
+			</span>
+            </dd>
+        <?php endif; ?>
+    <?php endif; ?>
+
+    <?php if ($this->contact->email_to && $this->params->get('show_email')) : ?>
+        <dt>
+		<span class="<?php echo $this->params->get('marker_class'); ?>" itemprop="email">
+			<?php echo nl2br($this->params->get('marker_email')); ?>
 		</span>
-	</dt>
-	<dd>
+        </dt>
+        <dd>
 		<span class="contact-emailto">
 			<?php echo $this->contact->email_to; ?>
 		</span>
-	</dd>
-<?php endif; ?>
+        </dd>
+    <?php endif; ?>
 
-<?php if ($this->contact->telephone && $this->params->get('show_telephone')) : ?>
-	<dt>
-		<span class="<?php echo $this->params->get('marker_class'); ?>" >
-			<i class="icon-phone"></i> <?php /*echo $this->params->get('marker_telephone'); Wright v.3: replaced image for icon */ ?>
+    <?php if ($this->contact->telephone && $this->params->get('show_telephone')) : ?>
+        <dt>
+		<span class="<?php echo $this->params->get('marker_class'); ?>">
+			<?php echo $this->params->get('marker_telephone'); ?>
 		</span>
-	</dt>
-	<dd>
-		<span class="contact-telephone">
-			<?php echo nl2br($this->contact->telephone); ?>
+        </dt>
+        <dd>
+		<span class="contact-telephone" itemprop="telephone">
+			<?php echo $this->contact->telephone; ?>
 		</span>
-	</dd>
-<?php endif; ?>
-<?php if ($this->contact->fax && $this->params->get('show_fax')) : ?>
-	<dt>
-		<span class="<?php echo $this->params->get('marker_class'); ?>" >
-			<i class="icon-print"></i> <?php /*echo $this->params->get('marker_fax'); Wright v.3: replaced image for icon */ ?>
+        </dd>
+    <?php endif; ?>
+    <?php if ($this->contact->fax && $this->params->get('show_fax')) : ?>
+        <dt>
+		<span class="<?php echo $this->params->get('marker_class'); ?>">
+			<?php echo $this->params->get('marker_fax'); ?>
 		</span>
-	</dt>
-	<dd>
-		<span class="contact-fax">
-		<?php echo nl2br($this->contact->fax); ?>
+        </dt>
+        <dd>
+		<span class="contact-fax" itemprop="faxNumber">
+		<?php echo $this->contact->fax; ?>
 		</span>
-	</dd>
-<?php endif; ?>
-<?php if ($this->contact->mobile && $this->params->get('show_mobile')) :?>
-	<dt>
-		<span class="<?php echo $this->params->get('marker_class'); ?>" >
-			<i class="icon-mobile-phone"></i> <?php /*echo $this->params->get('marker_mobile'); Wright v.3: replaced image for icon */ ?>
+        </dd>
+    <?php endif; ?>
+    <?php if ($this->contact->mobile && $this->params->get('show_mobile')) : ?>
+        <dt>
+		<span class="<?php echo $this->params->get('marker_class'); ?>">
+			<?php echo $this->params->get('marker_mobile'); ?>
 		</span>
-	</dt>
-	<dd>
-		<span class="contact-mobile">
-			<?php echo nl2br($this->contact->mobile); ?>
+        </dt>
+        <dd>
+		<span class="contact-mobile" itemprop="telephone">
+			<?php echo $this->contact->mobile; ?>
 		</span>
-	</dd>
-<?php endif; ?>
-<?php if ($this->contact->webpage && $this->params->get('show_webpage')) : ?>
-	<dt>
-		<span class="<?php echo $this->params->get('marker_class'); ?>" >
-			<i class="icon-home"></i> <?php /* Wright v.3: added icon */ ?>
+        </dd>
+    <?php endif; ?>
+    <?php if ($this->contact->webpage && $this->params->get('show_webpage')) : ?>
+        <dt>
+		<span class="<?php echo $this->params->get('marker_class'); ?>">
 		</span>
-	</dt>
-	<dd>
+        </dt>
+        <dd>
 		<span class="contact-webpage">
-			<a href="<?php echo $this->contact->webpage; ?>" target="_blank">
-			<?php echo JStringPunycode::urlToUTF8($this->contact->webpage); ?></a>
+			<a href="<?php echo $this->contact->webpage; ?>" target="_blank" rel="noopener noreferrer" itemprop="url">
+                <?php echo JStringPunycode::urlToUTF8($this->contact->webpage); ?></a>
 		</span>
-	</dd>
-<?php endif; ?>
+        </dd>
+    <?php endif; ?>
 </dl>
