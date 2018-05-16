13b77002c3db9b80cb1c3c04cc22d2f5fde970c6
Valentín García <htmgarcia@gmail.com>
2018-05-16 13:55:08 -0500

    Update contact category override

--- src/wright/html/joomla_4.0/com_contact/category/default_children.php
+++ src/wright/html/joomla_4.0/com_contact/category/default_children.php
@@ -1,5 +1,5 @@
 <?php
-// Wright v.3 Override: Joomla 3.2.2
+// Wright v.4 Override: Joomla 4.0
 /**
  * @package     Joomla.Site
  * @subpackage  com_contact
@@ -9,48 +9,51 @@
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\Component\Contact\Site\Helper\Route as ContactHelperRoute;
+
 $class = ' class="first"';
-if (count($this->children[$this->category->id]) > 0 && $this->maxLevel != 0) :
-?>
-<ul class="unstyled list-condensed">  <?php // Wright v.3: Changed class list-striped for unstyled ?>
-<?php foreach ($this->children[$this->category->id] as $id => $child) : ?>
-	<?php
-	if ($this->params->get('show_empty_categories') || $child->numitems || count($child->getChildren())) :
-		if (!isset($this->children[$this->category->id][$id + 1]))
-		{
-			$class = ' class="last"';
-		}
-	?>
-	<li<?php echo $class; ?>>
-		<?php $class = ''; ?>
-			<h4 class="item-title">
-				<a href="<?php echo JRoute::_(ContactHelperRoute::getCategoryRoute($child->id)); ?>">
-				<?php echo $this->escape($child->title); ?>
-				</a>
+if ($this->maxLevel != 0 && count($this->children[$this->category->id]) > 0) :
+    ?>
+    <ul class="list-striped list-condensed">
+        <?php foreach ($this->children[$this->category->id] as $id => $child) : ?>
+            <?php
+            if ($this->params->get('show_empty_categories') || $child->numitems || count($child->getChildren())) :
+                if (!isset($this->children[$this->category->id][$id + 1]))
+                {
+                    $class = ' class="last"';
+                }
+                ?>
+                <li<?php echo $class; ?>>
+                    <?php $class = ''; ?>
+                    <h4 class="item-title">
+                        <a href="<?php echo JRoute::_(ContactHelperRoute::getCategoryRoute($child->id)); ?>">
+                            <?php echo $this->escape($child->title); ?>
+                        </a>
 
-				<?php if ($this->params->get('show_cat_items') == 1) :?>
-					<span class="badge badge-info pull-right" title="<?php echo JText::_('COM_CONTACT_CAT_NUM'); ?>"><?php echo $child->numitems; ?></span>
-				<?php endif; ?>
-			</h4>
+                        <?php if ($this->params->get('show_cat_items') == 1) : ?>
+                            <span class="badge badge-info float-right" title="<?php echo JText::_('COM_CONTACT_CAT_NUM'); ?>"><?php echo $child->numitems; ?></span>
+                        <?php endif; ?>
+                    </h4>
 
-			<?php if ($this->params->get('show_subcat_desc') == 1) : ?>
-				<?php if ($child->description) : ?>
-					<div class="category-desc">
-						<?php echo JHtml::_('content.prepare', $child->description, '', 'com_contact.category'); ?>
-					</div>
-				<?php endif; ?>
-			<?php endif; ?>
+                    <?php if ($this->params->get('show_subcat_desc') == 1) : ?>
+                        <?php if ($child->description) : ?>
+                            <div class="category-desc">
+                                <?php echo JHtml::_('content.prepare', $child->description, '', 'com_contact.category'); ?>
+                            </div>
+                        <?php endif; ?>
+                    <?php endif; ?>
 
-			<?php if (count($child->getChildren()) > 0 ) :
-				$this->children[$child->id] = $child->getChildren();
-				$this->category = $child;
-				$this->maxLevel--;
-				echo $this->loadTemplate('children');
-				$this->category = $child->getParent();
-				$this->maxLevel++;
-			endif; ?>
-	</li>
-	<?php endif; ?>
-<?php endforeach; ?>
-</ul>
+                    <?php if (count($child->getChildren()) > 0 ) :
+                        $this->children[$child->id] = $child->getChildren();
+                        $this->category = $child;
+                        $this->maxLevel--;
+                        echo $this->loadTemplate('children');
+                        $this->category = $child->getParent();
+                        $this->maxLevel++;
+                    endif; ?>
+                </li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
 <?php endif; ?>
