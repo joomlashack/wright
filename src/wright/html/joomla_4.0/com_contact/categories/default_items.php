079ef3e948c5f4cd712daa69d3f26bff46179710
Valentín García <htmgarcia@gmail.com>
2018-05-16 13:49:11 -0500

    New contact categories override

--- src/wright/html/joomla_4.0/com_contact/categories/default_items.php
+++ src/wright/html/joomla_4.0/com_contact/categories/default_items.php
@@ -1,5 +1,5 @@
 <?php
-// Wright v.3 Override: Joomla 3.2.2
+// Wright v.4 Override: Joomla 4.0
 /**
  * @package     Joomla.Site
  * @subpackage  com_contact
@@ -10,54 +10,56 @@
 
 defined('_JEXEC') or die;
 
-JHtml::_('bootstrap.tooltip');
+use Joomla\Component\Contact\Site\Helper\Route as ContactHelperRoute;
 
 $class = ' class="first"';
-if (count($this->items[$this->parent->id]) > 0 && $this->maxLevelcat != 0) :
-?>
-	<?php foreach($this->items[$this->parent->id] as $id => $item) : ?>
-		<?php
-		if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) :
-			if (!isset($this->items[$this->parent->id][$id + 1]))
-			{
-				$class = ' class="last"';
-			}
-			?>
-			<div <?php echo $class; ?> >
-			<?php $class = ''; ?>
-				<h3 class="item-title"> <?php // Wright v.3: Removed page-header ?>
-					<a href="<?php echo JRoute::_(ContactHelperRoute::getCategoryRoute($item->id)); ?>">
-					<i class="icon-folder-open"> </i>  <?php // Wright v.3: Icon ?>
-					<?php echo $this->escape($item->title); ?></a>
-					<?php if ($this->params->get('show_cat_items_cat') == 1) : // Wright v.3: fixed show_cat_items_cat parameter ?>
-						<span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::tooltipText('COM_CONTACT_COUNT'); ?>"> <?php // Wright v.3: fixed COM_CONTACT_COUNT text ?>
-							<?php echo $item->numitems; ?>
+if ($this->maxLevelcat != 0 && count($this->items[$this->parent->id]) > 0) :
+    ?>
+    <?php foreach ($this->items[$this->parent->id] as $id => $item) : ?>
+    <?php
+    if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) :
+        if (!isset($this->items[$this->parent->id][$id + 1]))
+        {
+            $class = ' class="last"';
+        }
+        ?>
+        <div <?php echo $class; ?> >
+            <?php $class = ''; ?>
+            <h3 class="page-header item-title">
+                <a href="<?php echo JRoute::_(ContactHelperRoute::getCategoryRoute($item->id, $item->language)); ?>">
+                    <?php echo $this->escape($item->title); ?></a>
+                <?php if ($this->params->get('show_cat_items_cat') == 1) :?>
+                    <span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::_('tooltipText', 'COM_CONTACT_NUM_ITEMS'); ?>">
+							<?php echo JText::_('COM_CONTACT_NUM_ITEMS'); ?>&nbsp;
+                        <?php echo $item->numitems; ?>
 						</span>
-					<?php endif; ?>
-					<?php if (count($item->getChildren()) > 0) : ?>
-						<a href="#category-<?php echo $item->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
-					<?php endif;?>
-				</h3>
-				<?php if ($this->params->get('show_subcat_desc_cat') == 1) :?>
-					<?php if ($item->description) : ?>
-						<div class="category-desc">
-							<?php echo JHtml::_('content.prepare', $item->description, '', 'com_contact.categories'); ?>
-						</div>
-					<?php endif; ?>
-				<?php endif; ?>
+                <?php endif; ?>
+                <?php if ($this->maxLevelcat > 1 && count($item->getChildren()) > 0) : ?>
+                    <a id="category-btn-<?php echo $item->id; ?>" href="#category-<?php echo $item->id; ?>"
+                       data-toggle="collapse" data-toggle="button" class="btn btn-xs float-right" aria-label="<?php echo JText::_('JGLOBAL_EXPAND_CATEGORIES'); ?>"><span class="icon-plus" aria-hidden="true"></span></a>
+                <?php endif; ?>
+            </h3>
+            <?php if ($this->params->get('show_subcat_desc_cat') == 1) : ?>
+                <?php if ($item->description) : ?>
+                    <div class="category-desc">
+                        <?php echo JHtml::_('content.prepare', $item->description, '', 'com_contact.categories'); ?>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
 
-				<?php if (count($item->getChildren()) > 0) :?>
-					<div class="collapse fade" id="category-<?php echo $item->id;?>">
-						<?php
-						$this->items[$item->id] = $item->getChildren();
-						$this->parent = $item;
-						$this->maxLevelcat--;
-						echo $this->loadTemplate('items');
-						$this->parent = $item->getParent();
-						$this->maxLevelcat++;
-						?>
-					</div>
-				<?php endif; ?>
-			</div>
-		<?php endif; ?>
-	<?php endforeach; ?><?php endif; ?>
+            <?php if ($this->maxLevelcat > 1 && count($item->getChildren()) > 0) : ?>
+                <div class="collapse fade" id="category-<?php echo $item->id; ?>">
+                    <?php
+                    $this->items[$item->id] = $item->getChildren();
+                    $this->parent = $item;
+                    $this->maxLevelcat--;
+                    echo $this->loadTemplate('items');
+                    $this->parent = $item->getParent();
+                    $this->maxLevelcat++;
+                    ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
+<?php endforeach; ?><?php endif; ?>
+
