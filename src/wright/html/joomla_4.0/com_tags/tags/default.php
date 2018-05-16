5665409e53b80ae982168ffc2a1fb5eb777584c0
Valentín García <htmgarcia@gmail.com>
2018-05-16 16:42:47 -0500

    Safe override for tags component

---
+++ src/wright/html/joomla_4.0/com_tags/tags/default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_tags
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Joomlashack. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+require(JPATH_BASE . '/components/com_tags/tmpl/tags/default.php');
