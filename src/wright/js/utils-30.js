029d28ff4a1a1c7c05dfc74529b5527ce5fe4dfd
Valentín García <htmgarcia@gmail.com>
2018-07-26 15:37:44 -0500

    Fix utils for J!3

--- src/wright/js/utils-30.js
+++ src/wright/js/utils-30.js
@@ -1,70 +1,70 @@
 var disableToolbarResize = false;
 
-if (typeof jQuery != 'undefined' && typeof MooTools != 'undefined' ) { 
-	// fix for Bootstrap Carousel - conflicting with mootools-more
-	(function($) {
-		    Element.implement({
-		        slide: function(how, mode){
-		            return this;
-		        }
-		    });
-    	})(jQuery);
+if (typeof jQuery != 'undefined' && typeof MooTools != 'undefined' ) {
+    // fix for Bootstrap Carousel - conflicting with mootools-more
+    (function($) {
+        Element.implement({
+            slide: function(how, mode){
+                return this;
+            }
+        });
+    })(jQuery);
 }
 
 (function($) {
-	function wToolbar() {
-		if (typeof wrightWrapperToolbar === 'undefined')
-			wrightWrapperToolbar = '.wrapper-toolbar';
-			
-		$(wrightWrapperToolbar).each(function() {
-			$(this).css('min-height',$(this).find('.navbar-expand-lg').height() + 'px');
-		});
-	}
-	
-	function fixImagesIE() {
-		$('img').each(function() {
-			if ($(this).attr('width') != undefined)
-				$(this).width($(this).attr('width'));
-		});
-	}
-	
-	wToolbar();
-	fixImagesIE();
+    function wToolbar() {
+        if (typeof wrightWrapperToolbar === 'undefined')
+            wrightWrapperToolbar = '.wrapper-toolbar';
 
-	$(window).on('load', function () {
-		if (!disableToolbarResize)
-			wToolbar();
-	});
-	$(window).resize(function() {
-		if (!disableToolbarResize)
-			wToolbar();
-	});
+        $(wrightWrapperToolbar).each(function() {
+            $(this).css('min-height',$(this).find('.navbar:first').height() + 'px');
+        });
+    }
 
-	// Hover change images
-	$('img.wrightHover').mouseenter(function() {
-		if ($(this).data('wrighthover') != '') {
-			$(this).attr('src', $(this).data('wrighthover'));
-		}
-	}).mouseleave(function() {
-		if ($(this).data('wrighthover') != '') {
-			$(this).attr('src', $(this).data('wrighthoverorig'));
-		}
-	});
+    function fixImagesIE() {
+        $('img').each(function() {
+            if ($(this).attr('width') != undefined)
+                $(this).width($(this).attr('width'));
+        });
+    }
 
-	// Hover change images
-	$('img.wrightHoverNewsflash').closest('div').parent().mouseenter(function() {
-		var img = $(this).find('img.wrightHoverNewsflash');
-		if (img) {
-			if (img.data('wrighthover') != '') {
-				img.attr('src', img.data('wrighthover'));
-			}
-		}
-	}).mouseleave(function() {
-		var img = $(this).find('img.wrightHoverNewsflash');
-		if (img) {
-			if (img.data('wrighthover') != '') {
-				img.attr('src', img.data('wrighthoverorig'));
-			}
-		}
-	});
+    wToolbar();
+    fixImagesIE();
+
+    $(window).load(function () {
+        if (!disableToolbarResize)
+            wToolbar();
+    });
+    $(window).resize(function() {
+        if (!disableToolbarResize)
+            wToolbar();
+    });
+
+    // Hover change images
+    $('img.wrightHover').mouseenter(function() {
+        if ($(this).data('wrighthover') != '') {
+            $(this).attr('src', $(this).data('wrighthover'));
+        }
+    }).mouseleave(function() {
+        if ($(this).data('wrighthover') != '') {
+            $(this).attr('src', $(this).data('wrighthoverorig'));
+        }
+    });
+
+    // Hover change images
+    $('img.wrightHoverNewsflash').closest('div').parent().mouseenter(function() {
+        var img = $(this).find('img.wrightHoverNewsflash');
+        if (img) {
+            if (img.data('wrighthover') != '') {
+                img.attr('src', img.data('wrighthover'));
+            }
+        }
+    }).mouseleave(function() {
+        var img = $(this).find('img.wrightHoverNewsflash');
+        if (img) {
+            if (img.data('wrighthover') != '') {
+                img.attr('src', img.data('wrighthoverorig'));
+            }
+        }
+    });
 })(jQuery);
