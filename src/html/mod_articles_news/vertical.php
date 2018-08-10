95d8c1b5271d2462431ff72abdda10006676169c
Valentín García <htmgarcia@gmail.com>
2018-08-10 14:45:12 -0500

    Support intro image from the module parameter in Articles Newsflash

--- src/html/mod_articles_news/vertical.php
+++ src/html/mod_articles_news/vertical.php
@@ -11,7 +11,8 @@
 defined('_JEXEC') or die;
 $app = JFactory::getApplication();
 
-$wrightMaxColumns = 4;  // Max number of columns in horizontal layout
+$wrightMaxColumns       = 4;  // Max number of columns in horizontal layout
+$wrightEnableIntroText  = $params->get('show_introtext', 1); // Display intro text
 
 require_once(JPATH_THEMES.'/'.$app->getTemplate().'/'.'wright'.'/'.'html'.'/'.'overrider.php');
 require(Overrider::getOverride('mod_articles_news','vertical'));
