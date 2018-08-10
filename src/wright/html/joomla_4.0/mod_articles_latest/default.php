1385b28b1a99a1e4d24b82e3664c284f8cbd4096
Valentín García <htmgarcia@gmail.com>
2018-08-10 13:05:04 -0500

    Articles - Latest module tweaks with BS4 markup

--- src/wright/html/joomla_4.0/mod_articles_latest/default.php
+++ src/wright/html/joomla_4.0/mod_articles_latest/default.php
@@ -9,10 +9,10 @@
 
 defined('_JEXEC') or die;
 ?>
-<ul class="latestnews<?php echo $moduleclass_sfx; ?><?php if ($wrightAddNavs) : ?> nav nav-list<?php endif; ?>">  <?php // Wright v.4: Added optional nav nav-list classes ?>
+<ul class="latestnews<?php echo $moduleclass_sfx; ?><?php if ($wrightAddNavs) : ?> nav flex-column<?php endif; ?>">  <?php // Wright v.4: Added optional nav flex-column classes ?>
 <?php foreach ($list as $item) :  ?>
-	<li>
-		<a href="<?php echo $item->link; ?>">
+	<li class="nav-item">
+		<a href="<?php echo $item->link; ?>" class="nav-link">
 			<?php
 				/* Wright v.4: Add icon (optional) */
 				if ($wrightAddIcon)
