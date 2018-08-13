b22ae26674c925ccaae4d04996d532a255e4a982
Valentín García <htmgarcia@gmail.com>
2018-08-13 10:28:26 -0500

    Fix markup for article newsflash default layout

--- src/wright/html/joomla_4.0/mod_articles_news/default.php
+++ src/wright/html/joomla_4.0/mod_articles_news/default.php
@@ -10,10 +10,10 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Helper\ModuleHelper;
 ?>
-<div class="mod-articlesnews newsflash nav flex-column"> <?php // Wright v.4: Added classes nav flex-column ?>
+<ul class="mod-articlesnews newsflash nav flex-column"> <?php // Wright v.4: Added classes nav flex-column ?>
 	<?php foreach ($list as $item) : ?>
 		<li class="nav-item">
 			<?php require ModuleHelper::getLayoutPath('mod_articles_news', '_item'); ?>
 		</li>
 	<?php endforeach; ?>
-</div>
+</ul>
