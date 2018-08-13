0a767a58c0497e58f7515535b2164e7954d6d58f
Valentín García <htmgarcia@gmail.com>
2018-08-13 16:15:40 -0500

    New markup for Search module

--- src/wright/html/joomla_4.0/mod_search/default.php
+++ src/wright/html/joomla_4.0/mod_search/default.php
@@ -9,58 +9,36 @@
 
 defined('_JEXEC') or die;
 
-/* Wright v.4: Custom Image for style */
-	if ($button && $imagebutton) {
-		$app = JFactory::getApplication();
-		$objTemplate = $app->getTemplate(true);
-		$user = JFactory::getUser();
-		$style = JFactory::getApplication()->input->get('templateTheme',$user->getParam('theme',$objTemplate->params->get('style','generic')));
-		if (file_exists(JPATH_SITE . '/templates/' . $objTemplate->template . '/images/' . $style . '/searchButton.gif')) {
-			$img = JURI::base() . '/templates/' . $objTemplate->template . '/images/' . $style . '/searchButton.gif';
-		}
-	}
-
-/* End Wright v.4: Custom Image for style */
-
+use Joomla\CMS\Router\Route;
 ?>
-<div class="search<?php echo $moduleclass_sfx ?>">
-	<form action="<?php echo JRoute::_('index.php');?>" method="post" class="form-inline">
+
+<div class="mod-search search">
+	<form action="<?php echo Route::_('index.php'); ?>" method="post">
 		<?php
-			$output = '<label for="mod-search-searchword" class="element-invisible">' . $label . '</label> ';
-			$output .= '<input name="searchword" id="mod-search-searchword" maxlength="' . $maxlength . '"  class="inputbox search-query" type="text" size="' . $width . '" value="' . $text . '"  onblur="if (this.value==\'\') this.value=\'' . $text . '\';" onfocus="if (this.value==\'' . $text . '\') this.value=\'\';" />';
+		$input  = '<input name="searchword" id="mod-search-searchword' . $module->id . '" class="form-control" type="search" placeholder="' . $text . '">';
+		$output = '';
 
-			if ($button) :
-				if ($imagebutton) :
-					$btn_output = ' <input type="image" value="' . $button_text . '" class="button" src="' . $img . '" onclick="this.form.searchword.focus();"/>';
-				else :
-					$btn_output = ' <button class="button btn" onclick="this.form.searchword.focus();">' . $button_text . '</button>';  // Wright v.4: Removed btn-primary class
-				endif;
-
-				switch ($button_pos) :
-					case 'top' :
-						$output = $btn_output . '<br />' . $output;
-						break;
-
-					case 'bottom' :
-						$output .= '<br />' . $btn_output;
-						break;
-
-					case 'right' :
-						$output .= $btn_output;
-						break;
-
-					case 'left' :
-					default :
-						$output = $btn_output . $output;
-						break;
-				endswitch;
-
+		if ($button) :
+			if ($imagebutton) :
+				$btn_output = '<input type="image" alt="' . $button_text . '" class="btn btn-primary" src="' . $img . '" onclick="this.form.searchword.focus();">';
+			else :
+				$btn_output = '<button class="btn btn-primary" onclick="this.form.searchword.focus();">' . $button_text . '</button>';
 			endif;
 
-			echo $output;
+			$output .= '<div class="input-group">';
+			$output .= $input;
+			$output .= '<span class="input-group-append">';
+			$output .= $btn_output;
+			$output .= '</span>';
+			$output .= '</div>';
+		else :
+			$output .= $input;
+		endif;
+
+		echo $output;
 		?>
-		<input type="hidden" name="task" value="search" />
-		<input type="hidden" name="option" value="com_search" />
-		<input type="hidden" name="Itemid" value="<?php echo $mitemid; ?>" />
+		<input type="hidden" name="option" value="com_search">
+		<input type="hidden" name="task" value="search">
+		<input type="hidden" name="Itemid" value="<?php echo $mitemid; ?>">
 	</form>
 </div>
