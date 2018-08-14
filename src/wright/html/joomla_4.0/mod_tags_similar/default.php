6b0c0d268e6008e926037e096f674db7b2aef9cb
Valentín García <htmgarcia@gmail.com>
2018-08-14 09:56:35 -0500

    New markup for Tags - Similar module

--- src/wright/html/joomla_4.0/mod_tags_similar/default.php
+++ src/wright/html/joomla_4.0/mod_tags_similar/default.php
@@ -13,11 +13,12 @@ defined('_JEXEC') or die;
 <?php JLoader::register('TagsHelperRoute', JPATH_BASE . '/components/com_tags/helpers/route.php'); ?>
 <div class="tagssimilar<?php echo $moduleclass_sfx; ?>">
 <?php if ($list) : ?>
-	<ul class="nav nav-list" > <?php // Wright v.4: Nav List ?>
+	<ul class="nav flex-column">
 	<?php foreach ($list as $i => $item) : ?>
-		<li>
+		<li class="nav-item">
 			<?php $item->route = new JHelperRoute; ?>
-			<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
+			<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>"
+				class="nav-link">
 				<i class="icon-file icons-left"></i>  <?php // Wright v.4: Added icon ?>
 				<?php if (!empty($item->core_title)) :
 					echo htmlspecialchars($item->core_title);
