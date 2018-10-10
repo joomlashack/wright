6ee683806092c419974ec28bd5b5f95a81285351
Valentín García <htmgarcia@gmail.com>
2018-10-10 14:42:28 -0500

    Call core layouts for mod_menu

--- src/html/com_kunena/layouts/widget/menu/default_separator.php
+++ src/html/com_kunena/layouts/widget/menu/default_separator.php
@@ -9,28 +9,4 @@
 
 defined('_JEXEC') or die;
 
-$title      = $item->anchor_title ? ' title="' . $item->anchor_title . '"' : '';
-$anchor_css = $item->anchor_css ?: '';
-
-$linktype   = $item->title;
-
-if ($item->menu_image)
-{
-	if ($item->menu_image_css)
-	{
-		$image_attributes['class'] = $item->menu_image_css;
-		$linktype = JHtml::_('image', $item->menu_image, $item->title, $image_attributes);
-	}
-	else
-	{
-		$linktype = JHtml::_('image', $item->menu_image, $item->title);
-	}
-
-	if ($item->params->get('menu_text', 1))
-	{
-		$linktype .= '<span class="image-title">' . $item->title . '</span>';
-	}
-}
-
-?>
-<span class="separator <?php echo $anchor_css; ?>"<?php echo $title; ?>><?php echo $linktype; ?></span>
+require(JPATH_BASE . '/modules/mod_menu/tmpl/default_separator.php');
