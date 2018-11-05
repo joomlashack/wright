768ef5f463b97cb7d0f96b0ccf273c04e81defb7
Valentín García <htmgarcia@gmail.com>
2018-11-05 17:30:04 -0600

    Switch logo scrape to template params

--- src/wright/html/joomla_3.0/com_content/com_content.helper.php
+++ src/wright/html/joomla_3.0/com_content/com_content.helper.php
@@ -57,7 +57,7 @@ function getIntroImageFloat($item) {
 
 function scrapeSiteLogo() {
 
-	if (function_exists('file_get_contents')) {
+	/*if (function_exists('file_get_contents')) {
 
 		preg_match_all(
 			'|<div id=[\'"]logo[\'"].*?<img.*?src=[\'"](.*?)[\'"].*?>|ism',
@@ -73,6 +73,78 @@ function scrapeSiteLogo() {
 	else
 	{
 		$site_logo = '';
+	}
+
+	return $site_logo;*/
+
+	$app        = JFactory::getApplication();
+	$template   = $app->getTemplate(true);
+	$site_logo  = $template->params->get('logo','template');
+
+	switch($site_logo) {
+
+		// Image from images/ folder
+		default:
+			$site_logo = JURI::root() . 'images/' . $site_logo;
+			break;
+
+		// Logo image from template, template style or Wright framework
+		case 'template':
+
+			$template_style = $template->params->get('style');
+
+			if (is_file(JPATH_ROOT . '/templates/' . $app->getTemplate() . '/images/' . $template_style . '/logo.png'))
+			{
+				$site_logo = JURI::root() . 'templates/' . $app->getTemplate() . '/images/' . $template_style . '/logo.png';
+			}
+			elseif (is_file(JPATH_ROOT . '/templates/' . $app->getTemplate() . '/images/' . $template_style . '/logo.png'))
+			{
+				$site_logo = JURI::root() . 'templates/' . $app->getTemplate() . '/images/' . $template_style . '/logo.png';
+			}
+			elseif (is_file(JPATH_ROOT . '/' . 'templates/' . $app->getTemplate() . '/images/logo.png'))
+			{
+				$site_logo = JURI::root() . 'templates/' . $app->getTemplate() . '/images/logo.png';
+			}
+			elseif (is_file(JPATH_ROOT . '/' . 'templates/' . $app->getTemplate() . '/images/logo.png'))
+			{
+				$site_logo = JURI::root() . 'templates/' . $app->getTemplate() . '/images/logo.png';
+			}
+			else
+			{
+				$site_logo = JURI::root() . 'templates/' . $app->getTemplate() . '/wright/images/logo.png';
+			}
+			break;
+
+		// Extract image from a published module in "logo" position
+		case 'module':
+
+			$db = JFactory::getDbo();
+
+			$query = $db->getQuery(true);
+			$query->select($db->quoteName('content'))
+				->from($db->quoteName('#__modules'))
+				->where(
+					$db->quoteName('position') . ' LIKE '. $db->quote('logo')
+					. ' AND ' . $db->quoteName('published') . ' = '. $db->quote(1)
+					. ' AND ' . $db->quoteName('module') . ' LIKE '. $db->quote('mod_custom')
+				)
+				->setLimit('1');
+			$db->setQuery($query);
+			$logo_module = $db->loadResult();
+
+			preg_match_all(
+				'|<img.*?src=[\'"](.*?)[\'"].*?>|i',
+				$logo_module,
+				$matches
+			);
+
+			$site_logo = JURI::base() . str_replace(JURI::base(), '', $matches[1][0]);
+			break;
+
+		// No logo image! Just text
+		case 'title':
+			$site_logo = '';
+			break;
 	}
 
 	return $site_logo;
