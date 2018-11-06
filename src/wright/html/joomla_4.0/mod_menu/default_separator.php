1656c202f441aee52a12d664d783fb3c5785c524
Valentín García <htmgarcia@gmail.com>
2018-11-06 15:28:43 -0600

    Revert "Set initial values for vars in separator menu items"
    This reverts commit 76ebf205f49af7450ceebb1faba7a02813576d8f.

--- src/wright/html/joomla_4.0/mod_menu/default_separator.php
+++ src/wright/html/joomla_4.0/mod_menu/default_separator.php
@@ -9,12 +9,6 @@
 
 defined('_JEXEC') or die;
 
-// Wright v.3: Created additional structure for icons
-$structIcons    = '';
-$span1          = '';
-$span2          = '';
-$hidden         = '';
-
 // Note. It is important to remove spaces between elements.
 $title = $item->anchor_title ? ' title="' . $item->anchor_title . '" ' : '';
 
