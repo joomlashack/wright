cd7956d1a0ba8ca2f68040c4cc003eed88b4dd0a
Valentín García <htmgarcia@gmail.com>
2018-11-06 15:24:47 -0600

    Fix broken HTML

--- src/wright/html/joomla_4.0/com_content/article/default.php
+++ src/wright/html/joomla_4.0/com_content/article/default.php
@@ -291,7 +291,7 @@ foreach ($this->wrightElementsStructure as $wrightElement) :
                 <?php if (isset ($this->item->toc)) :
                     echo wrightTransformArticleTOC($this->item->toc);  // Wright v.4: TOC transformation (using helper)
                 endif; ?>
-                    <div itemprop="articleBody"> class="mb-5">
+                    <div itemprop="articleBody" class="mb-5">
                         <?php echo wrightTransformArticleContent($this->item->text);  // Wright v.4: Transform article content's plugins (using helper) ?>
                     </div>
             <?php
