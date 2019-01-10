b6d2c1b40c2e66867372eb840690df9313e4cdec
Valentín García <htmgarcia@gmail.com>
2019-01-10 09:41:30 -0600

    Setting up ajax method to compile LESS

---
+++ src/wright/parameters/assets/compilecss.js
@@ -0,0 +1,16 @@
+jQuery(function ($) {
+    $('#wCompileCssBtn').on('click', function (event) {
+        event.preventDefault();
+
+        $.ajax(this.href, {
+            success: function(data) {
+                console.log('success');
+                $('#wCompileCssStatus').html('<div class="alert alert-info">' + data + ' - Success!</div>');
+            },
+            error: function() {
+                console.log('error');
+                $('#wCompileCssStatus').html('<div class="alert alert-warning">' + data + ' - Error!</div>');
+            }
+        });
+    });
+});
---
+++ src/wright/parameters/joomla_3.0/compilecss.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package     Wright
+ * @subpackage  Parameters
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Joomlashack.   All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Restrict Access to within Joomla
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Wright button to trigger less compiler
+ *
+ * @package     Wright
+ * @subpackage  Parameters
+ * @since       3.9.1
+ */
+class JFormFieldCompilecss extends JFormField
+{
+	protected $type = 'Compilecss';
+
+	/**
+	 * Creates the Compilecss button field
+	 *
+	 * @return  JFormField  Formatted input
+	 */
+	protected function getInput()
+	{
+        $doc        = JFactory::getDocument();
+        $template   = $this->form->getValue('template');
+        $doc->addScript(str_replace('/administrator/', '/', JURI::base()) . 'templates/' . $template . '/wright/parameters/assets/compilecss.js');
+
+        $html  = '<a class="btn btn-primary" id="wCompileCssBtn" href="' . str_replace('/administrator/', '/', JURI::base()) . 'templates/' . $template . '/wright/build/build.php">' . JText::_('Compile') . '</a>';
+        $html .= '<div id="wCompileCssStatus"></div>';
+
+        return $html;
+	}
+
+	/**
+	 * Method to get the field label markup.
+	 *
+	 * @return  string  The field label markup.
+	 */
+	protected function getLabel()
+	{
+		return '';
+	}
+}
