0721360a44be433a7150f2b08a153d887d0e335b
Valentín García <htmgarcia@gmail.com>
2019-04-24 18:04:56 -0500

    Fix pagination

--- src/wright/html/joomla_4.0/layouts/joomla/pagination/link.php
+++ src/wright/html/joomla_4.0/layouts/joomla/pagination/link.php
@@ -10,81 +10,87 @@
 defined('JPATH_BASE') or die;
 
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Layout\LayoutHelper;
-use Joomla\Registry\Registry;
 
 $item    = $displayData['data'];
 $display = $item->text;
 
 switch ((string) $item->text)
 {
-	// Check for "Start" item
-	case Text::_('JLIB_HTML_START') :
-		$icon = 'fa fa-angle-double-left';
-		break;
+    // Check for "Start" item
+    case Text::_('JLIB_HTML_START') :
+        $icon = 'fa fa-angle-double-left';
+        $aria = Text::sprintf('JLIB_HTML_GOTO_POSITION', strtolower($item->text));
+        break;
 
-	// Check for "Prev" item
-	case $item->text === Text::_('JPREV') :
-		$item->text = Text::_('JPREVIOUS');
-		$icon = 'fa fa-angle-left';
-		break;
+    // Check for "Prev" item
+    case $item->text === Text::_('JPREV') :
+        $item->text = Text::_('JPREVIOUS');
+        $icon = 'fa fa-angle-left';
+        $aria =Text::sprintf('JLIB_HTML_GOTO_POSITION', strtolower($item->text));
+        break;
 
-	// Check for "Next" item
-	case Text::_('JNEXT') :
-		$icon = 'fa fa-angle-right';
-		break;
+    // Check for "Next" item
+    case Text::_('JNEXT') :
+        $icon = 'fa fa-angle-right';
+        $aria = Text::sprintf('JLIB_HTML_GOTO_POSITION', strtolower($item->text));
+        break;
 
-	// Check for "End" item
-	case Text::_('JLIB_HTML_END') :
-		$icon = 'fa fa-angle-double-right';
-		break;
+    // Check for "End" item
+    case Text::_('JLIB_HTML_END') :
+        $icon = 'fa fa-angle-double-right';
+        $aria = Text::sprintf('JLIB_HTML_GOTO_POSITION', strtolower($item->text));
+        break;
 
-	default:
-		$icon = null;
-		break;
+    default:
+        $icon = null;
+        $aria = Text::sprintf('JLIB_HTML_GOTO_PAGE', strtolower($item->text));
+        break;
 }
 
 if ($icon !== null)
 {
-	$display = '<span class="' . $icon . '" aria-hidden="true"></span>';
+    $display = '<span class="' . $icon . '" aria-hidden="true"></span>';
 }
 
 if ($displayData['active'])
 {
-	if ($item->base > 0)
-	{
-		$limit = 'limitstart.value=' . $item->base;
-	}
-	else
-	{
-		$limit = 'limitstart.value=0';
-	}
+    if ($item->base > 0)
+    {
+        $limit = 'limitstart.value=' . $item->base;
+    }
+    else
+    {
+        $limit = 'limitstart.value=0';
+    }
 
-	$cssClasses = array();
-
-	$title = '';
-
-	if (!is_numeric($item->text))
-	{
-		$cssClasses[] = 'hasTooltip';
-		$title = ' title="' . $item->text . '" ';
-	}
-
-	$onClick = 'document.adminForm.' . $item->prefix . 'limitstart.value=' . ($item->base > 0 ? $item->base : '0') . '; Joomla.submitform();return false;';
+    $class = 'active';
+    $title = 'title="' . $item->text . '"';
+    $onClick = 'document.adminForm.' . $item->prefix . 'limitstart.value=' . ($item->base > 0 ? $item->base : '0') . '; Joomla.submitform();return false;';
 }
 else
 {
-	$class = (property_exists($item, 'active') && $item->active) ? 'active' : 'disabled';
+    $class = (property_exists($item, 'active') && $item->active) ? 'active' : 'disabled';
 }
 ?>
 <?php if ($displayData['active']) : ?>
-	<li>
-		<a <?php echo $cssClasses ? 'class="' . implode(' ', $cssClasses) . '"' : ''; ?> <?php echo $title; ?> href="#" onclick="<?php echo $onClick; ?>">
-			<?php echo $display; ?>
-		</a>
-	</li>
+    <li class="<?php echo $class; ?> page-item">
+        <a class="page-link" <?php echo $title; ?> aria-label="<?php echo $aria; ?>" href="#" onclick="<?php echo $onClick; ?>">
+            <?php echo $display; ?>
+        </a>
+    </li>
+<?php elseif (isset($item->active) && $item->active) : ?>
+    <?php $aria = Text::sprintf('JLIB_HTML_PAGE_CURRENT', strtolower($item->text)); ?>
+    <li class="<?php echo $class; ?> page-item current">
+        <span class="page-link">
+            <span aria-current="true" aria-label="<?php echo $aria; ?>">
+                <?php echo $display; ?>
+            </span>
+        </span>
+    </li>
 <?php else : ?>
-	<li class="<?php echo $class; ?>">
-		<span><?php echo $display; ?></span>
-	</li>
-<?php endif;
+    <li class="<?php echo $class; ?> page-item">
+        <span class="page-link">
+            <?php echo $display; ?>
+        </span>
+    </li>
+<?php endif; ?>
