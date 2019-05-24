b57b79749c0ccccb8e8ea6bd14e2ae03c542ba9f
Valentín García <htmgarcia@gmail.com>
2019-05-24 14:07:36 -0500

    Categories component base override

--- src/wright/html/joomla_4.0/com_content/categories/default.php
+++ src/wright/html/joomla_4.0/com_content/categories/default.php
@@ -10,5 +10,52 @@
 // No direct access.
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_content/tmpl/categories/default.php');
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
+
+HTMLHelper::_('behavior.core');
+
+// Add strings for translations in Javascript.
+Text::script('JGLOBAL_EXPAND_CATEGORIES');
+Text::script('JGLOBAL_COLLAPSE_CATEGORIES');
+
+$js = <<<JS
+(function() {
+	document.addEventListener('DOMContentLoaded', function() {
+		var categories = [].slice.call(document.querySelectorAll('.categories-list'));
+
+		categories.forEach(function(category) {
+			var buttons = [].slice.call(document.querySelectorAll('.categories-list'));
+
+			buttons.forEach(function(button) {
+				var span = button.querySelector('span');
+
+				if(span) {
+				  span.classList.toggle('fas fa-plus')
+				  span.classList.toggle('fas fa-minus')
+				}
+
+				if (button.getAttribute('aria-label') === Joomla.JText._('JGLOBAL_EXPAND_CATEGORIES'))
+				{
+					button.setAttribute('aria-label', Joomla.JText._('JGLOBAL_COLLAPSE_CATEGORIES'));
+				} else {
+					button.setAttribute('aria-label', Joomla.JText._('JGLOBAL_EXPAND_CATEGORIES'));
+				}
+			})
+	  })
+	});
+})();
+JS;
+
+// @todo move script to a file
+Factory::getDocument()->addScriptDeclaration($js);
+?>
+<div class="com-content-categories categories-list">
+    <?php
+    echo LayoutHelper::render('joomla.content.categories_default', $this);
+    echo $this->loadTemplate('items');
+    ?>
+</div>
 
