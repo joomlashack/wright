2df6c1ed1a542d4168c5e31f95bc72a983a0481e
Valentín García <htmgarcia@gmail.com>
2019-05-24 15:39:28 -0500

    Login / Logout component layouts

--- src/wright/html/joomla_4.0/com_users/login/default_login.php
+++ src/wright/html/joomla_4.0/com_users/login/default_login.php
@@ -9,4 +9,94 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_users/tmpl/login/default_login.php');
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Router\Route;
+
+HTMLHelper::_('behavior.keepalive');
+HTMLHelper::_('behavior.formvalidator');
+
+$usersConfig = ComponentHelper::getParams('com_users');
+
+?>
+<div class="com-users-login login">
+    <?php if ($this->params->get('show_page_heading')) : ?>
+        <div class="page-header">
+            <h1>
+                <?php echo $this->escape($this->params->get('page_heading')); ?>
+            </h1>
+        </div>
+    <?php endif; ?>
+
+    <?php if (($this->params->get('logindescription_show') == 1 && str_replace(' ', '', $this->params->get('login_description')) != '') || $this->params->get('login_image') != '') : ?>
+    <div class="com-users-login__description login-description mb-3">
+        <?php endif; ?>
+
+        <?php if ($this->params->get('logindescription_show') == 1) : ?>
+            <?php echo $this->params->get('login_description'); ?>
+        <?php endif; ?>
+
+        <?php if ($this->params->get('login_image') != '') : ?>
+            <img src="<?php echo $this->escape($this->params->get('login_image')); ?>" class="com-users-login__image float-right login-image" alt="<?php echo Text::_('COM_USERS_LOGIN_IMAGE_ALT'); ?>">
+            <div class="clearfix"></div>
+        <?php endif; ?>
+
+        <?php if (($this->params->get('logindescription_show') == 1 && str_replace(' ', '', $this->params->get('login_description')) != '') || $this->params->get('login_image') != '') : ?>
+    </div>
+<?php endif; ?>
+
+    <form action="<?php echo Route::_('index.php?option=com_users&task=user.login'); ?>" method="post" class="com-users-login__form form-validate form-horizontal well">
+
+        <div class="card">
+            <div class="card-body">
+                <?php echo $this->form->renderFieldset('credentials', ['class' => 'com-users-login__input']); ?>
+
+                <?php if ($this->tfa) : ?>
+                    <?php echo $this->form->renderField('secretkey', ['class' => 'com-users-login__secretkey']); ?>
+                <?php endif; ?>
+
+                <?php if (PluginHelper::isEnabled('system', 'remember')) : ?>
+                    <div  class="com-users-login__remember control-group">
+                        <div class="control-label">
+                            <label for="remember">
+                                <?php echo Text::_('COM_USERS_LOGIN_REMEMBER_ME'); ?>
+                            </label>
+                        </div>
+                        <div class="controls">
+                            <input id="remember" type="checkbox" name="remember" class="inputbox" value="yes">
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+                <div class="com-users-login__submit control-group">
+                    <div class="controls">
+                        <button type="submit" class="btn btn-primary">
+                            <?php echo Text::_('JLOGIN'); ?>
+                        </button>
+                    </div>
+                </div>
+
+                <?php $return = $this->form->getValue('return', '', $this->params->get('login_redirect_url', $this->params->get('login_redirect_menuitem'))); ?>
+                <input type="hidden" name="return" value="<?php echo base64_encode($return); ?>">
+                <?php echo HTMLHelper::_('form.token'); ?>
+            </div>
+        </div>
+    </form>
+</div>
+<div>
+    <div class="com-users-login__options list-group">
+        <a class="com-users-login__reset list-group-item list-group-item-action" href="<?php echo Route::_('index.php?option=com_users&view=reset'); ?>">
+            <?php echo Text::_('COM_USERS_LOGIN_RESET'); ?>
+        </a>
+        <a class="com-users-login__remind list-group-item list-group-item-action" href="<?php echo Route::_('index.php?option=com_users&view=remind'); ?>">
+            <?php echo Text::_('COM_USERS_LOGIN_REMIND'); ?>
+        </a>
+        <?php if ($usersConfig->get('allowUserRegistration')) : ?>
+            <a class="com-users-login__register list-group-item list-group-item-action" href="<?php echo Route::_('index.php?option=com_users&view=registration'); ?>">
+                <?php echo Text::_('COM_USERS_LOGIN_REGISTER'); ?>
+            </a>
+        <?php endif; ?>
+    </div>
+</div>
--- src/wright/html/joomla_4.0/com_users/login/default_logout.php
+++ src/wright/html/joomla_4.0/com_users/login/default_logout.php
@@ -9,4 +9,51 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_users/tmpl/login/default_logout.php');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
+?>
+<div class="com-users-logout logout">
+    <?php if ($this->params->get('show_page_heading')) : ?>
+        <div class="page-header">
+            <h1>
+                <?php echo $this->escape($this->params->get('page_heading')); ?>
+            </h1>
+        </div>
+    <?php endif; ?>
+
+    <?php if (($this->params->get('logoutdescription_show') == 1 && str_replace(' ', '', $this->params->get('logout_description')) != '')|| $this->params->get('logout_image') != '') : ?>
+    <div class="com-users-logout__description logout-description mb-3">
+        <?php endif; ?>
+
+        <?php if ($this->params->get('logoutdescription_show') == 1) : ?>
+            <?php echo $this->params->get('logout_description'); ?>
+        <?php endif; ?>
+
+        <?php if ($this->params->get('logout_image') != '') : ?>
+            <img src="<?php echo $this->escape($this->params->get('logout_image')); ?>" class="com-users-logout__image thumbnail float-right logout-image" alt="<?php echo Text::_('COM_USERS_LOGOUT_IMAGE_ALT'); ?>">
+            <div class="clearfix"></div>
+        <?php endif; ?>
+
+        <?php if (($this->params->get('logoutdescription_show') == 1 && str_replace(' ', '', $this->params->get('logout_description')) != '')|| $this->params->get('logout_image') != '') : ?>
+    </div>
+<?php endif; ?>
+
+    <form action="<?php echo Route::_('index.php?option=com_users&task=user.logout'); ?>" method="post" class="com-users-logout__form form-horizontal well">
+        <div class="com-users-login__submit card">
+            <div class="card-body">
+                <button type="submit" class="btn btn-primary">
+                    <span class="fas fa-arrow-left"></span>
+                    <?php echo Text::_('JLOGOUT'); ?>
+                </button>
+            </div>
+        </div>
+        <?php if ($this->params->get('logout_redirect_url')) : ?>
+            <input type="hidden" name="return" value="<?php echo base64_encode($this->params->get('logout_redirect_url', $this->form->getValue('return'))); ?>">
+        <?php else : ?>
+            <input type="hidden" name="return" value="<?php echo base64_encode($this->params->get('logout_redirect_menuitem', $this->form->getValue('return'))); ?>">
+        <?php endif; ?>
+        <?php echo HTMLHelper::_('form.token'); ?>
+    </form>
+</div>
