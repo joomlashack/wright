9ee7a8e63cdebab00ed451c11198f02b75e77f97
Valentín García <htmgarcia@gmail.com>
2019-05-27 16:45:11 -0500

    User profile override

--- src/wright/html/joomla_4.0/com_users/profile/default_core.php
+++ src/wright/html/joomla_4.0/com_users/profile/default_core.php
@@ -9,4 +9,44 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_users/tmpl/profile/default_core.php');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+?>
+<fieldset id="com-users-profile__core users-profile-core" class="mb-4">
+    <legend>
+        <?php echo Text::_('COM_USERS_PROFILE_CORE_LEGEND'); ?>
+    </legend>
+    <dl class="dl-horizontal">
+        <dt>
+            <?php echo Text::_('COM_USERS_PROFILE_NAME_LABEL'); ?>
+        </dt>
+        <dd>
+            <?php echo $this->escape($this->data->name); ?>
+        </dd>
+        <dt>
+            <?php echo Text::_('COM_USERS_PROFILE_USERNAME_LABEL'); ?>
+        </dt>
+        <dd>
+            <?php echo $this->escape($this->data->username); ?>
+        </dd>
+        <dt>
+            <?php echo Text::_('COM_USERS_PROFILE_REGISTERED_DATE_LABEL'); ?>
+        </dt>
+        <dd>
+            <?php echo HTMLHelper::_('date', $this->data->registerDate, Text::_('DATE_FORMAT_LC1')); ?>
+        </dd>
+        <dt>
+            <?php echo Text::_('COM_USERS_PROFILE_LAST_VISITED_DATE_LABEL'); ?>
+        </dt>
+        <?php if ($this->data->lastvisitDate != $this->db->getNullDate()) : ?>
+            <dd>
+                <?php echo HTMLHelper::_('date', $this->data->lastvisitDate, Text::_('DATE_FORMAT_LC1')); ?>
+            </dd>
+        <?php else : ?>
+            <dd>
+                <?php echo Text::_('COM_USERS_PROFILE_NEVER_VISITED'); ?>
+            </dd>
+        <?php endif; ?>
+    </dl>
+</fieldset>
--- src/wright/html/joomla_4.0/com_users/profile/default_params.php
+++ src/wright/html/joomla_4.0/com_users/profile/default_params.php
@@ -9,4 +9,33 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_users/tmpl/profile/default_params.php');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+?>
+<?php $fields = $this->form->getFieldset('params'); ?>
+<?php if (count($fields)) : ?>
+    <fieldset id="users-profile-custom" class="com-users-profile__params mb-4">
+        <legend><?php echo Text::_('COM_USERS_SETTINGS_FIELDSET_LABEL'); ?></legend>
+        <dl class="dl-horizontal">
+            <?php foreach ($fields as $field) : ?>
+                <?php if (!$field->hidden) : ?>
+                    <dt>
+                        <?php echo $field->title; ?>
+                    </dt>
+                    <dd>
+                        <?php if (HTMLHelper::isRegistered('users.' . $field->id)) : ?>
+                            <?php echo HTMLHelper::_('users.' . $field->id, $field->value); ?>
+                        <?php elseif (HTMLHelper::isRegistered('users.' . $field->fieldname)) : ?>
+                            <?php echo HTMLHelper::_('users.' . $field->fieldname, $field->value); ?>
+                        <?php elseif (HTMLHelper::isRegistered('users.' . $field->type)) : ?>
+                            <?php echo HTMLHelper::_('users.' . $field->type, $field->value); ?>
+                        <?php else : ?>
+                            <?php echo HTMLHelper::_('users.value', $field->value); ?>
+                        <?php endif; ?>
+                    </dd>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </dl>
+    </fieldset>
+<?php endif; ?>
