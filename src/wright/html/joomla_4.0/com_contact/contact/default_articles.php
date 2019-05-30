e8e36dee4a39c17abad17da8db28d7f32c1a2356
Valentín García <htmgarcia@gmail.com>
2019-05-30 10:32:13 -0500

    Single contact override

--- src/wright/html/joomla_4.0/com_contact/contact/default_articles.php
+++ src/wright/html/joomla_4.0/com_contact/contact/default_articles.php
@@ -9,4 +9,20 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_contact/tmpl/contact/default_articles.php');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
+
+JLoader::register('ContentHelperRoute', JPATH_SITE . '/components/com_content/helpers/route.php');
+
+?>
+<?php if ($this->params->get('show_articles')) : ?>
+    <div class="com-contact__articles contact-articles">
+        <ul class="nav nav-tabs nav-stacked">
+            <?php foreach ($this->item->articles as $article) : ?>
+                <li>
+                    <?php echo HTMLHelper::_('link', Route::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language)), htmlspecialchars($article->title, ENT_COMPAT, 'UTF-8')); ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
--- src/wright/html/joomla_4.0/com_contact/contact/default_links.php
+++ src/wright/html/joomla_4.0/com_contact/contact/default_links.php
@@ -9,4 +9,35 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_contact/tmpl/contact/default_links.php');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+?>
+<?php echo '<h3>' . Text::_('COM_CONTACT_LINKS') . '</h3>'; ?>
+
+<div class="com-contact__links contact-links">
+    <ul class="nav flex-column">
+        <?php
+        // Letters 'a' to 'e'
+        foreach (range('a', 'e') as $char) :
+            $link = $this->item->params->get('link' . $char);
+            $label = $this->item->params->get('link' . $char . '_name');
+
+            if (!$link) :
+                continue;
+            endif;
+
+            // Add 'http://' if not present
+            $link = (0 === strpos($link, 'http')) ? $link : 'http://' . $link;
+
+            // If no label is present, take the link
+            $label = $label ?: $link;
+            ?>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $link; ?>" itemprop="url">
+                    <?php echo $label; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
