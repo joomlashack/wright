85c7037811ceab9cb09ffe65ab58e7aa3a62c4f3
Valentín García <htmgarcia@gmail.com>
2019-07-03 08:01:09 +0900

    Remove obsolete icon actions from articles

--- src/wright/html/joomla_4.0/layouts/joomla/content/icons.php
+++ src/wright/html/joomla_4.0/layouts/joomla/content/icons.php
@@ -11,34 +11,15 @@ defined('JPATH_BASE') or die;
 
 $canEdit = $displayData['params']->get('access-edit');
 
+if ($canEdit) :
 ?>
-
-<div class="icons">
-	<?php if (empty($displayData['print'])) : ?>
-
-		<?php if ($canEdit || $displayData['params']->get('show_print_icon') || $displayData['params']->get('show_email_icon')) : ?>
-			<div class="btn-group float-right icons-actions">   <?php // Wright v.4: Added icons-actions class ?>
-				<a class="btn btn-secondary btn-sm dropdown-toggle" data-toggle="dropdown" href="#"> <span class="fas fa-cog"></span> <span class="caret"></span> </a>
-				<?php // Note the actions class is deprecated. Use dropdown-menu instead. ?>
-				<ul class="dropdown-menu">
-					<?php if ($displayData['params']->get('show_print_icon')) : ?>
-						<li class="print-icon"> <?php echo JHtml::_('icon.print_popup', $displayData['item'], $displayData['params']); ?> </li>
-					<?php endif; ?>
-					<?php if ($displayData['params']->get('show_email_icon')) : ?>
-						<li class="email-icon"> <?php echo JHtml::_('icon.email', $displayData['item'], $displayData['params']); ?> </li>
-					<?php endif; ?>
-					<?php if ($canEdit) : ?>
-						<li class="edit-icon"> <?php echo JHtml::_('icon.edit', $displayData['item'], $displayData['params']); ?> </li>
-					<?php endif; ?>
-				</ul>
-			</div>
-		<?php endif; ?>
-
-	<?php else : ?>
-
-		<div class="float-right">
-			<?php echo JHtml::_('icon.print_screen', $displayData['item'], $displayData['params']); ?>
-		</div>
-
-	<?php endif; ?>
-</div>
+    <div class="icons">
+        <div class="btn-group float-right icons-actions">   <?php // Wright v.4: Added icons-actions class ?>
+            <a class="btn btn-secondary btn-sm dropdown-toggle" data-toggle="dropdown" href="#"> <span class="fas fa-cog"></span> <span class="caret"></span> </a>
+            <?php // Note the actions class is deprecated. Use dropdown-menu instead. ?>
+            <ul class="dropdown-menu">
+                <li class="edit-icon"> <?php echo JHtml::_('icon.edit', $displayData['item'], $displayData['params']); ?> </li>
+            </ul>
+        </div>
+    </div>
+<?php endif; ?>
