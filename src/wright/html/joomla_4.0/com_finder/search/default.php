58ef8a7ce6999d1c6a53d8ace858dfd38640ef27
Valentín García <htmgarcia@gmail.com>
2019-08-20 21:15:13 +0900

    Smart search override tweaks

--- src/wright/html/joomla_4.0/com_finder/search/default.php
+++ src/wright/html/joomla_4.0/com_finder/search/default.php
@@ -9,4 +9,39 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_finder/tmpl/search/default.php');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+HTMLHelper::_('behavior.core');
+HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+
+Text::script('MOD_FINDER_SEARCH_VALUE', true);
+
+HTMLHelper::_('stylesheet', 'com_finder/finder.css', array('version' => 'auto', 'relative' => true));
+HTMLHelper::_('script', 'com_finder/finder.js', array('version' => 'auto', 'relative' => true));
+
+?>
+<div class="com-finder finder">
+    <?php if ($this->params->get('show_page_heading')) : ?>
+        <div class="page-header">
+            <h1>
+                <?php if ($this->escape($this->params->get('page_heading'))) : ?>
+                    <?php echo $this->escape($this->params->get('page_heading')); ?>
+                <?php else : ?>
+                    <?php echo $this->escape($this->params->get('page_title')); ?>
+                <?php endif; ?>
+            </h1>
+        </div>
+    <?php endif; ?>
+    <?php if ($this->params->get('show_search_form', 1)) : ?>
+        <div id="search-form" class="com-finder__form">
+            <?php echo $this->loadTemplate('form'); ?>
+        </div>
+    <?php endif; ?>
+    <?php // Load the search results layout if we are performing a search. ?>
+    <?php if ($this->query->search === true) : ?>
+        <div id="search-results" class="com-finder__results">
+            <?php echo $this->loadTemplate('results'); ?>
+        </div>
+    <?php endif; ?>
+</div>
