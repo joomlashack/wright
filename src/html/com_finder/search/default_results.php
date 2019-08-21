9c81031447279857323a030f4a69e404b327f11c
Valentín García <htmgarcia@gmail.com>
2019-08-21 21:03:58 +0900

    Smart search results layout improvements

---
+++ src/html/com_finder/search/default_results.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package     Wright
+ * @subpackage  Overrider
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Joomlashack.   All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+$app = JFactory::getApplication();
+
+require_once(JPATH_THEMES.'/'.$app->getTemplate().'/'.'wright'.'/'.'html'.'/'.'overrider.php');
+include(Overrider::getOverride('com_finder.search','default_results'));
+?>
