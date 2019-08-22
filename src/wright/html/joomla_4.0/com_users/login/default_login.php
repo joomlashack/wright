8fa7c2c014a09bc6c586a85672ecb5a3f428281d
Valentín García <htmgarcia@gmail.com>
2019-08-22 21:37:01 +0900

    Minor design improvement for login view

--- src/wright/html/joomla_4.0/com_users/login/default_login.php
+++ src/wright/html/joomla_4.0/com_users/login/default_login.php
@@ -49,39 +49,37 @@ $usersConfig = ComponentHelper::getParams('com_users');
 
     <form action="<?php echo Route::_('index.php?option=com_users&task=user.login'); ?>" method="post" class="com-users-login__form form-validate form-horizontal well">
 
-        <div class="card">
-            <div class="card-body">
-                <?php echo $this->form->renderFieldset('credentials', ['class' => 'com-users-login__input']); ?>
+        <div class="card-body bg-light">
+            <?php echo $this->form->renderFieldset('credentials', ['class' => 'com-users-login__input']); ?>
 
-                <?php if ($this->tfa) : ?>
-                    <?php echo $this->form->renderField('secretkey', ['class' => 'com-users-login__secretkey']); ?>
-                <?php endif; ?>
+            <?php if ($this->tfa) : ?>
+                <?php echo $this->form->renderField('secretkey', ['class' => 'com-users-login__secretkey']); ?>
+            <?php endif; ?>
 
-                <?php if (PluginHelper::isEnabled('system', 'remember')) : ?>
-                    <div  class="com-users-login__remember control-group">
-                        <div class="control-label">
-                            <label for="remember">
-                                <?php echo Text::_('COM_USERS_LOGIN_REMEMBER_ME'); ?>
-                            </label>
-                        </div>
-                        <div class="controls">
-                            <input id="remember" type="checkbox" name="remember" class="inputbox" value="yes">
-                        </div>
+            <?php if (PluginHelper::isEnabled('system', 'remember')) : ?>
+                <div  class="com-users-login__remember control-group">
+                    <div class="control-label">
+                        <label for="remember">
+                            <?php echo Text::_('COM_USERS_LOGIN_REMEMBER_ME'); ?>
+                        </label>
                     </div>
-                <?php endif; ?>
-
-                <div class="com-users-login__submit control-group">
                     <div class="controls">
-                        <button type="submit" class="btn btn-primary">
-                            <?php echo Text::_('JLOGIN'); ?>
-                        </button>
+                        <input id="remember" type="checkbox" name="remember" class="inputbox" value="yes">
                     </div>
                 </div>
+            <?php endif; ?>
 
-                <?php $return = $this->form->getValue('return', '', $this->params->get('login_redirect_url', $this->params->get('login_redirect_menuitem'))); ?>
-                <input type="hidden" name="return" value="<?php echo base64_encode($return); ?>">
-                <?php echo HTMLHelper::_('form.token'); ?>
+            <div class="com-users-login__submit control-group">
+                <div class="controls">
+                    <button type="submit" class="btn btn-primary">
+                        <?php echo Text::_('JLOGIN'); ?>
+                    </button>
+                </div>
             </div>
+
+            <?php $return = $this->form->getValue('return', '', $this->params->get('login_redirect_url', $this->params->get('login_redirect_menuitem'))); ?>
+            <input type="hidden" name="return" value="<?php echo base64_encode($return); ?>">
+            <?php echo HTMLHelper::_('form.token'); ?>
         </div>
     </form>
 </div>
