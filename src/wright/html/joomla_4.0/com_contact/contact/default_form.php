8883927a8e21b206878873332d2dea13b4b4bf21
Valentín García <htmgarcia@gmail.com>
2019-08-23 09:31:50 +0900

    Design touch for single contact view

--- src/wright/html/joomla_4.0/com_contact/contact/default_form.php
+++ src/wright/html/joomla_4.0/com_contact/contact/default_form.php
@@ -18,32 +18,34 @@ HTMLHelper::_('behavior.formvalidator');
 
 ?>
 <div class="com-contact__form contact-form mb-5">
-    <form id="contact-form" action="<?php echo Route::_('index.php'); ?>" method="post" class="form-validate form-horizontal well">
-        <?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
-            <?php if ($fieldset->name === 'captcha' && !$this->captchaEnabled) : ?>
-                <?php continue; ?>
-            <?php endif; ?>
-            <?php $fields = $this->form->getFieldset($fieldset->name); ?>
-            <?php if (count($fields)) : ?>
-                <fieldset>
+    <form id="contact-form" action="<?php echo Route::_('index.php'); ?>" method="post" class="form-validate form-horizontal">
+        <fieldset class="card-body bg-light">
+            <?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
+                <?php if ($fieldset->name === 'captcha' && !$this->captchaEnabled) : ?>
+                    <?php continue; ?>
+                <?php endif; ?>
+                <?php $fields = $this->form->getFieldset($fieldset->name); ?>
+                <?php if (count($fields)) : ?>
                     <?php if (isset($fieldset->label) && ($legend = trim(Text::_($fieldset->label))) !== '') : ?>
-                        <legend><?php echo $legend; ?></legend>
+                        <div>
+                            <legend><?php echo $legend; ?></legend>
+                        </div>
                     <?php endif; ?>
                     <?php foreach ($fields as $field) : ?>
                         <?php echo $field->renderField(); ?>
                     <?php endforeach; ?>
-                </fieldset>
-            <?php endif; ?>
-        <?php endforeach; ?>
-        <div class="control-group">
-            <div class="controls">
-                <button class="btn btn-primary validate" type="submit"><?php echo Text::_('COM_CONTACT_CONTACT_SEND'); ?></button>
-                <input type="hidden" name="option" value="com_contact">
-                <input type="hidden" name="task" value="contact.submit">
-                <input type="hidden" name="return" value="<?php echo $this->return_page; ?>">
-                <input type="hidden" name="id" value="<?php echo $this->item->slug; ?>">
-                <?php echo HTMLHelper::_('form.token'); ?>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <div class="control-group mb-0">
+                <div class="controls">
+                    <button class="btn btn-primary validate" type="submit"><?php echo Text::_('COM_CONTACT_CONTACT_SEND'); ?></button>
+                    <input type="hidden" name="option" value="com_contact">
+                    <input type="hidden" name="task" value="contact.submit">
+                    <input type="hidden" name="return" value="<?php echo $this->return_page; ?>">
+                    <input type="hidden" name="id" value="<?php echo $this->item->slug; ?>">
+                    <?php echo HTMLHelper::_('form.token'); ?>
+                </div>
             </div>
-        </div>
+        </fieldset>
     </form>
 </div>
