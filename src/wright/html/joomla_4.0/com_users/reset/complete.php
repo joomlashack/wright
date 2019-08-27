ef949ba9b4cee66ad6b78c398647d2b2c9cf62d3
Valentín García <htmgarcia@gmail.com>
2019-08-27 20:14:38 +0900

    Reset password layout

--- src/wright/html/joomla_4.0/com_users/reset/complete.php
+++ src/wright/html/joomla_4.0/com_users/reset/complete.php
@@ -9,4 +9,39 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_users/tmpl/reset/complete.php');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
+HTMLHelper::_('behavior.keepalive');
+HTMLHelper::_('behavior.formvalidator');
+
+?>
+<div class="com-users-reset-complete reset-complete">
+    <?php if ($this->params->get('show_page_heading')) : ?>
+        <div class="page-header">
+            <h1>
+                <?php echo $this->escape($this->params->get('page_heading')); ?>
+            </h1>
+        </div>
+    <?php endif; ?>
+    <form action="<?php echo Route::_('index.php?option=com_users&task=reset.complete'); ?>" method="post" class="com-users-reset-complete__form form-validate form-horizontal well">
+        <div class="card-body bg-light">
+            <?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
+                <?php if (isset($fieldset->label)) : ?>
+                    <p><?php echo Text::_($fieldset->label); ?></p>
+                <?php endif; ?>
+                <?php echo $this->form->renderFieldset($fieldset->name); ?>
+            <?php endforeach; ?>
+            <div class="com-users-reset-complete__submit control-group mb-0">
+                <div class="controls">
+                    <button type="submit" class="btn btn-primary validate">
+                        <?php echo Text::_('JSUBMIT'); ?>
+                    </button>
+                </div>
+            </div>
+            <?php echo HTMLHelper::_('form.token'); ?>
+        </div>
+    </form>
+</div>
+
--- src/wright/html/joomla_4.0/com_users/reset/default.php
+++ src/wright/html/joomla_4.0/com_users/reset/default.php
@@ -9,4 +9,40 @@
 
 defined('_JEXEC') or die;
 
-require(JPATH_BASE . '/components/com_users/tmpl/reset/default.php');
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
+HTMLHelper::_('behavior.keepalive');
+HTMLHelper::_('behavior.formvalidator');
+
+?>
+<div class="com-users-reset reset">
+    <?php if ($this->params->get('show_page_heading')) : ?>
+        <div class="page-header">
+            <h1>
+                <?php echo $this->escape($this->params->get('page_heading')); ?>
+            </h1>
+        </div>
+    <?php endif; ?>
+    <form id="user-registration" action="<?php echo Route::_('index.php?option=com_users&task=reset.request'); ?>" method="post" class="com-users-reset__form form-validate form-horizontal well">
+        <div class="card-body bg-light">
+            <?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
+
+                <?php if (isset($fieldset->label)) : ?>
+                    <p><?php echo Text::_($fieldset->label); ?></p>
+                <?php endif; ?>
+                <?php echo $this->form->renderFieldset($fieldset->name); ?>
+            <?php endforeach; ?>
+            <div class="com-users-reset__submit control-group mb-0">
+                <div class="controls">
+                    <button type="submit" class="btn btn-primary validate">
+                        <?php echo Text::_('JSUBMIT'); ?>
+                    </button>
+                </div>
+            </div>
+            <?php echo HTMLHelper::_('form.token'); ?>
+        </div>
+    </form>
+</div>
+
