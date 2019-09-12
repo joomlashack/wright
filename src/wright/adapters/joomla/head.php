ac7ab4ea94e46ccad73713b8563aa7eb2f37f373
Valentín García <htmgarcia@gmail.com>
2019-09-13 08:28:53 +0900

    Remove empty space

--- src/wright/adapters/joomla/head.php
+++ src/wright/adapters/joomla/head.php
@@ -7,7 +7,7 @@ class WrightAdapterJoomlaHead
 		$head = '';
 		$dochtml = JFactory::getDocument();
         // add viewport meta for tablets always
-        $head = '  <meta name="viewport" content="width=device-width, initial-scale=1.0">' . "\n";
+        $head = '<meta name="viewport" content="width=device-width, initial-scale=1.0">' . "\n";
 		$head .= '<jdoc:include type="head" />';
 	    $head .= "\n";
 		return $head;
