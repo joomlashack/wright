576b23beff1332d29e5184ff68629309e5ea7320
Valentín García <htmgarcia@gmail.com>
2020-01-16 20:31:51 +0900

    js code to convert radio buttons into bootstrap style

--- src/wright/js/utils.js
+++ src/wright/js/utils.js
@@ -69,6 +69,76 @@ if (typeof jQuery != 'undefined' && typeof MooTools != 'undefined' ) {
         }
     }
 
+    function wRadioButtons() {
+
+        // Taken from protostar template
+        "use strict";
+
+        $(document)
+            .on('click', ".btn-group label:not(.active)", function() {
+                var $label = $(this);
+                var $input = $('#' + $label.attr('for'));
+
+                if ($input.prop('checked')) {
+                    return;
+                }
+
+                $label.closest('.btn-group').find("label").removeClass('active btn-success btn-danger btn-primary');
+
+                var btnClass = 'primary';
+
+
+                if ($input.val() != '')
+                {
+                    var reversed = $label.closest('.btn-group').hasClass('btn-group-reversed');
+                    btnClass = ($input.val() == 0 ? !reversed : reversed) ? 'danger' : 'success';
+                }
+
+                $label.addClass('active btn-' + btnClass);
+                $input.prop('checked', true).trigger('change');
+            })
+            .on('click', '#back-top', function (e) {
+                e.preventDefault();
+                $("html, body").animate({scrollTop: 0}, 1000);
+            })
+            .on('subform-row-add', initButtonGroup)
+            .on('subform-row-add', initTooltip);
+
+        initButtonGroup();
+        initTooltip();
+
+        // Called once on domready, again when a subform row is added
+        function initTooltip(event, container)
+        {
+            $(container || document).find('*[rel=tooltip]').tooltip();
+        }
+
+        // Called once on domready, again when a subform row is added
+        function initButtonGroup(event, container)
+        {
+            var $container = $(container || document);
+
+            // Turn radios into btn-group
+            $container.find('.radio.btn-group label').addClass('btn');
+
+            $container.find(".btn-group input:checked").each(function()
+            {
+                var $input  = $(this);
+                var $label = $('label[for=' + $input.attr('id') + ']');
+                var btnClass = 'primary';
+
+                if ($input.val() != '')
+                {
+                    var reversed = $input.parent().hasClass('btn-group-reversed');
+                    btnClass = ($input.val() == 0 ? !reversed : reversed) ? 'danger' : 'success';
+                }
+
+                $label.addClass('active btn-' + btnClass);
+            });
+        }
+    }
+
+	wRadioButtons();
 	wToolbar();
 	fixImagesIE();
 
@@ -111,3 +181,9 @@ if (typeof jQuery != 'undefined' && typeof MooTools != 'undefined' ) {
 		}
 	});
 })(jQuery);
+
+
+jQuery(function($) {
+
+
+});
