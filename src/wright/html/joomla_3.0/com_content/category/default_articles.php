fd46034d29d1ae86147f23559211a309696892d5
Nick Savov <nicksavov@users.noreply.github.com>
2020-05-11 10:50:27 -0500

    Fixes Warning: Creating default object from empty value
    Should fix the article params too.

--- src/wright/html/joomla_3.0/com_content/category/default_articles.php
+++ src/wright/html/joomla_3.0/com_content/category/default_articles.php
@@ -15,8 +15,7 @@ JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 JHtml::_('behavior.framework');
 
 // Create some shortcuts.
-$params		= &$this->item->params;
-$n			= count($this->items);
+$n		= count($this->items);
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
 
@@ -183,7 +182,7 @@ if (!empty($this->items))
                 <?php if ($isEditable) : ?>
                     <td headers="categorylist_header_edit" class="list-edit">
                         <?php if ($article->params->get('access-edit')) : ?>
-                            <?php echo JHtml::_('icon.edit', $article, $params); ?>
+                            <?php echo JHtml::_('icon.edit', $article, $article->params); ?>
                         <?php endif; ?>
                     </td>
                 <?php endif; ?>
