f162c4815860074152e41c7d4155b465a4bba1be
Valentín García <htmgarcia@gmail.com>
2020-12-11 20:03:48 -0600

    Set custom number precision

--- src/wright/build/libraries/lessphp/lessc.inc.php
+++ src/wright/build/libraries/lessphp/lessc.inc.php
@@ -37,6 +37,8 @@
  * The `lessc_formatter` takes a CSS tree, and dumps it to a formatted string,
  * handling things like indentation.
  */
+ini_set('precision', 16);
+
 class lessc {
 	static public $VERSION = "v0.5.0";
 
